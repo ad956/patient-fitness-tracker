85f689ca2b20088f41c2d215eedce2c191d366da
Anand Suthar <anandsuthar956@gmail.com>
2024-02-29 18:34:17 +0530

    Added constants

---
+++ app/constants/index.ts
@@ -0,0 +1,19 @@
+const Data = [
+  {
+    image: "/reception.png",
+    title: "Efficient Patient Management",
+    desc: " Receptionist dashboard for streamlined appointment and record management",
+  },
+  {
+    image: "/doctor.png",
+    title: "Personalized Care Delivery",
+    desc: "Doctor interface for tailored treatment plans and patient care.",
+  },
+  {
+    image: "/admin.png",
+    title: "Comprehensive Admin Tools",
+    desc: "Admin dashboard for seamless hospital operations and performance monitoring.",
+  },
+];
+
+export { Data };
