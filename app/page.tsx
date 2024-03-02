9b7837b39a5692993d4fd3b236fea7b0b960ee4a
Anand Suthar <anandsuthar956@gmail.com>
2024-03-03 00:05:57 +0530

    Mobile nav initilised

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
 
 export default function Home() {
   return (
     <main className="m-2">
-      <div className="p-2 border-2 border-black rounded-b-xl border-t-0">
+      <div className="flex justify-between items-center p-2 border-2 border-black rounded-b-xl border-t-0">
         <div className="flex items-center gap-2">
           <Image src="patient.svg" height="30" width="30" alt="brand-logo" />
           <h2 className="font-bold">Patient Fitness Tracker</h2>
         </div>
+        <CiMenuFries className="" height="50" />
       </div>
-      <nav className="hidden ">
+      <nav className="hidden">
         <Link href="#home">Home</Link>
         <Link href="#team">Our Team</Link>
         <Link href="#service">Services</Link>
