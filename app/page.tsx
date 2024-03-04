19d683c6c2c32074842a530156893d23c588f893
Anand Suthar <anandsuthar956@gmail.com>
2024-03-04 23:46:55 +0530

    Added card to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <Card
           isFooterBlurred
           radius="lg"
-          className="border-none h-64  m-5 flex flex-col gap-5 items-center text-bold"
+          className="border-none h-[30vh]  m-5 flex flex-col gap-10 items-center text-bold"
         >
           <h3 className="p-2 text-4xl tracking-wide">
             The <span className="text-violet-600">New Era</span> of Healthcare
@@ -29,30 +29,20 @@ export default function Home() {
             facilities.
           </p>
 
-          <button
-            type="button"
-            className="text-sm font-bold border-2 border-blue-500 p-1  rounded-xl"
+          <Button
+            className="text-tiny text-white bg-black/20"
+            variant="flat"
+            color="default"
+            radius="lg"
+            size="sm"
           >
             Get Started
-          </button>
-          <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px) shadow-small ml-1 z-10">
-            <p className="text-tiny text-white/80">Available soon.</p>
-            <Button
-              className="text-tiny text-white bg-black/20"
-              variant="flat"
-              color="default"
-              radius="lg"
-              size="sm"
-            >
-              Notify me
-            </Button>
-          </CardFooter>
+          </Button>
         </Card>
       </section>
 
-      <section className="h-96 items-center flex flex-col">
+      <section className="h-[80vh] items-center flex flex-col">
         <Carousel data={Data} />
-
         <p className="p-2 font-bold">
           Trusted and used by over{" "}
           <span className="text-violet-600">1000+</span> clients
