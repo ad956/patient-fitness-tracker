9666091af6616f9d9a6003ef067702183b91c005
Anand Suthar <anandsuthar956@gmail.com>
2024-03-05 23:43:51 +0530

    minor

--- app/page.tsx
+++ app/page.tsx
@@ -3,45 +3,16 @@ import Carousel from "./components/carousel";
 import { Data } from "./constants";
 import { BrandLogo } from "./components/brandlogo";
 import NavBar from "./components/navbar";
+import Intro from "./components/intro";
 import { Card, CardFooter, Image, Button } from "@nextui-org/react";
+import Lifeline from "./components/lifeline";
 
 export default function Home() {
   return (
     <main className="h-[100vh]">
       <NavBar />
-      <section className="h-5/6 p-5 flex justify-center gap-10 items-center">
-        <div className="h-4/5 w-2/5 p-5 flex flex-col justify-center gap-10 items-center text-bold">
-          <h3 className="text-4xl font-medium tracking-wide">
-            The <span className="text-[#e95b7b]">New Era</span> of Healthcare
-            Management
-          </h3>
-
-          <p className="text-md font-medium tracking-wider">
-            The Patient Fitness Tracker introduces a new era of healthcare
-            management, offering a comprehensive platform to streamline patient
-            care, optimize operations, and enhance collaboration across medical
-            facilities.
-          </p>
-
-          <Button
-            className="self-start font-bold text-white/90 bg-[#e95b7b]"
-            variant="flat"
-            color="default"
-            radius="lg"
-            size="md"
-          >
-            Get Started
-          </Button>
-        </div>
-
-        <Card
-          isFooterBlurred
-          radius="lg"
-          className="h-5/6 w-2/5 p-5 flex justify-center items-center"
-        >
-          <Image src="admin.png" height="auto" width="100%" />
-        </Card>
-      </section>
+      <Intro />
+      <Lifeline />
 
       <section className="h-[80vh] items-center flex flex-col">
         <Carousel data={Data} />
