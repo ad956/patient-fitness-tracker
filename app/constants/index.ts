b12c72d0fc1d938e3e967ed79afa1a9075085022
Anand Suthar <anandsuthar956@gmail.com>
2024-03-08 11:29:39 +0530

    Added features

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,4 +1,4 @@
-const Data = [
+const carouselData = [
   {
     image: "/reception.png",
     title: "Efficient Patient Management",
@@ -16,4 +16,31 @@ const Data = [
   },
 ];
 
-export { Data };
+const projectFeatures = [
+  {
+    title: "Comprehensive Patient Management",
+    description:
+      "Efficiently manage patient information, appointments, and treatment plans, enhancing coordination and care delivery.",
+    icon: "https://skillicons.dev/icons?i=management&theme=light",
+  },
+  {
+    title: "Secure and Accessible Health Records",
+    description:
+      "Empower patients with secure access to their health records while enabling healthcare providers to retrieve and update information easily.",
+    icon: "https://skillicons.dev/icons?i=security&theme=light",
+  },
+  {
+    title: "QR Code Integration for Instant Access",
+    description:
+      "Assign unique QR codes to patients for quick access to their complete health records, enhancing efficiency and accuracy in patient care.",
+    icon: "https://skillicons.dev/icons?i=qr-code&theme=light",
+  },
+  {
+    title: "Real-time Communication and Alerts",
+    description:
+      "Facilitate real-time communication between patients and healthcare providers, ensuring timely appointment reminders, medication alerts, and notifications.",
+    icon: "https://skillicons.dev/icons?i=communication&theme=light",
+  },
+];
+
+export { carouselData, projectFeatures };
