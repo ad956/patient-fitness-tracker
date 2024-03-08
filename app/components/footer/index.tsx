df26563f0e28f2072da9f336dbc02a3574ba18dd
Anand Suthar <anandsuthar956@gmail.com>
2024-03-08 22:58:21 +0530

    Footer component initiated

---
+++ app/components/footer/index.tsx
@@ -0,0 +1,39 @@
+export default function Footer() {
+  return (
+    <div className="m-5">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo impedit
+      quae adipisci corrupti obcaecati laudantium, alias voluptatem officia quis
+      saepe earum assumenda numquam quisquam perspiciatis ipsa. Dolores fugit
+      consequuntur eaque. Illo labore delectus ad odit exercitationem debitis et
+      excepturi explicabo maiores, laborum dicta vitae nihil vero odio vel velit
+      non laudantium eligendi natus iure facilis! Sunt, suscipit. Laborum,
+      officia quibusdam. Corporis, adipisci. Libero omnis corrupti deleniti,
+      ducimus animi praesentium nihil a accusantium! Architecto veritatis
+      perferendis culpa consequuntur incidunt sequi, beatae maxime quod.
+      Expedita maiores magnam quaerat eveniet fugit, consequuntur similique.
+      Eligendi laboriosam officiis repellat? Odit tenetur illum distinctio eius
+      molestias porro et aliquam laudantium mollitia eaque consectetur, delectus
+      magnam, cumque eum minima laborum quibusdam? Quos veniam facere
+      repellendus est voluptas. Voluptatum numquam officia dignissimos et! Neque
+      ut totam aliquid numquam sint quisquam vitae quas sit beatae fugiat illum
+      atque iste nesciunt ab, eos voluptates modi, ratione fugit laborum nihil
+      et! Sint quae, labore quidem dolores, possimus quam nihil expedita illo
+      vero ullam a repudiandae dolore doloribus rerum eveniet totam nam
+      quibusdam molestias eos incidunt exercitationem corrupti excepturi ratione
+      magni! Expedita. Ipsa impedit, sequi aperiam, enim dicta quae voluptatem
+      eaque nihil libero voluptates voluptatum odio! Incidunt alias praesentium,
+      quo vel, dolores accusamus magni asperiores minima assumenda iusto quidem
+      aspernatur, dolor libero? A commodi, numquam placeat nulla vitae sed
+      cumque eligendi sapiente fugiat fuga rem facilis provident officia error
+      eius distinctio eveniet totam eaque quibusdam tempora quo velit! Natus non
+      saepe at. Beatae, nesciunt? Sunt similique laborum, quibusdam consectetur
+      repellendus nulla ad minima doloremque deleniti id accusamus alias
+      corporis inventore quod recusandae blanditiis. Minima laboriosam numquam
+      sint sapiente totam sit, deserunt eaque. Cumque perspiciatis ea
+      consequatur molestiae id, quaerat sint? Odit sequi illum vitae accusantium
+      architecto dolore sapiente exercitationem dolorum voluptatibus cum. Eum
+      ipsa explicabo molestias aspernatur sapiente? Recusandae incidunt numquam
+      aliquid!
+    </div>
+  );
+}
