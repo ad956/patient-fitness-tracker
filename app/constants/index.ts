d2d504b1daada7cb3eda08ab0af6a4b22de8d8d7
Anand Suthar <anandsuthar956@gmail.com>
2024-03-08 18:50:25 +0530

    Added data items for features component

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -21,25 +21,25 @@ const projectFeatures = [
     title: "Comprehensive Patient Management",
     description:
       "Efficiently manage patient information, appointments, and treatment plans, enhancing coordination and care delivery.",
-    icon: "https://skillicons.dev/icons?i=management&theme=light",
+    icon: "MdOutlineManageAccounts",
   },
   {
     title: "Secure and Accessible Health Records",
     description:
       "Empower patients with secure access to their health records while enabling healthcare providers to retrieve and update information easily.",
-    icon: "https://skillicons.dev/icons?i=security&theme=light",
+    icon: "MdOutlineDocumentScanner",
   },
   {
     title: "QR Code Integration for Instant Access",
     description:
       "Assign unique QR codes to patients for quick access to their complete health records, enhancing efficiency and accuracy in patient care.",
-    icon: "https://skillicons.dev/icons?i=qr-code&theme=light",
+    icon: "MdOutlineQrCodeScanner",
   },
   {
     title: "Real-time Communication and Alerts",
     description:
       "Facilitate real-time communication between patients and healthcare providers, ensuring timely appointment reminders, medication alerts, and notifications.",
-    icon: "https://skillicons.dev/icons?i=communication&theme=light",
+    icon: "CiCircleAlert",
   },
 ];
 
