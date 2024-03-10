a1af48976c4897916c096f7d11a7678ec4cf8af1
Anand Suthar <anandsuthar956@gmail.com>
2024-03-10 23:18:00 +0530

    Added modal for footer card links

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,4 +1,17 @@
-import { Card, Divider, Image } from "@nextui-org/react";
+"use client";
+
+import {
+	Button,
+	Card,
+	Divider,
+	Image,
+	Modal,
+	ModalBody,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	useDisclosure,
+} from "@nextui-org/react";
 import { TfiTwitter } from "react-icons/tfi";
 import {
 	FaTwitter,
@@ -8,23 +21,31 @@ import {
 } from "react-icons/fa6";
 
 export default function Footer() {
+	const { isOpen, onOpen, onOpenChange } = useDisclosure();
 	return (
 		<footer className="h-4/5 flex flex-col justify-center p-5">
 			<Card
 				isBlurred
 				shadow="lg"
 				radius="lg"
-				className="bg-[#1c1624] h-full flex flex-col justify-center items-center"
+				className="bg-[#1c1624] h-full flex flex-col justify-center items-center gap-5"
 			>
 				<div className="">fi</div>
 				<Divider className="bg-white/70 w-3/5" />
-				<div className="flex flex-col justify-center items-center">
-					<Image src="patient.svg" height={50} width={50} />
-					<p className="text-white/90">
-						Patient Fitness Tracker is revolutionizing healthcare engagement
-						through its comprehensive solution, encompassing Push Notifications,
-						Email, SMS, and In-App communication.
-					</p>
+				<div className="flex flex-col justify-center items-center gap-5 w-3/5">
+					<Image src="patient.svg" height={40} width={40} />
+					<div className="text-center text-white/90">
+						<p className="break-words">
+							<span className="block sm:inline">
+								Patient Fitness Tracker is revolutionizing healthcare engagement
+								with its comprehensive solution.
+							</span>
+							<span className="block sm:inline">
+								Offering Push Notifications, Email, SMS, and In-App
+								communication.
+							</span>
+						</p>
+					</div>
 
 					<div className="flex flex-row gap-5">
 						<FaLinkedin size={25} color="white" />
@@ -33,11 +54,84 @@ export default function Footer() {
 						<TfiTwitter size={25} color="white" />
 					</div>
 
-					<div className="flex flex-row text-white">
-						<p className="">Privacy</p>
-						<p className="">Terms of Use</p>
-						<p className="">Acceptable Use Policy</p>
-						<p className="">Software Lifecycle Policy</p>
+					<div className="flex flex-row  gap-5">
+						<Button className=" bg-transparent text-white" onPress={onOpen}>
+							Privacy
+						</Button>
+						<Button className=" bg-transparent text-white">Terms of Use</Button>
+						<Button className=" bg-transparent text-white">
+							Acceptable Use Policy
+						</Button>
+						<Button className=" bg-transparent text-white">
+							Software Lifecycle Policy
+						</Button>
+
+						<Modal
+							backdrop="opaque"
+							isOpen={isOpen}
+							onOpenChange={onOpenChange}
+							motionProps={{
+								variants: {
+									enter: {
+										y: 0,
+										opacity: 1,
+										transition: {
+											duration: 0.3,
+											ease: "easeOut",
+										},
+									},
+									exit: {
+										y: -20,
+										opacity: 0,
+										transition: {
+											duration: 0.2,
+											ease: "easeIn",
+										},
+									},
+								},
+							}}
+						>
+							<ModalContent>
+								{(onClose) => (
+									<>
+										<ModalHeader className="flex flex-col gap-1">
+											Modal Title
+										</ModalHeader>
+										<ModalBody>
+											<p>
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+												Nullam pulvinar risus non risus hendrerit venenatis.
+												Pellentesque sit amet hendrerit risus, sed porttitor
+												quam.
+											</p>
+											<p>
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+												Nullam pulvinar risus non risus hendrerit venenatis.
+												Pellentesque sit amet hendrerit risus, sed porttitor
+												quam.
+											</p>
+											<p>
+												Magna exercitation reprehenderit magna aute tempor
+												cupidatat consequat elit dolor adipisicing. Mollit dolor
+												eiusmod sunt ex incididunt cillum quis. Velit duis sit
+												officia eiusmod Lorem aliqua enim laboris do dolor
+												eiusmod. Et mollit incididunt nisi consectetur esse
+												laborum eiusmod pariatur proident Lorem eiusmod et.
+												Culpa deserunt nostrud ad veniam.
+											</p>
+										</ModalBody>
+										<ModalFooter>
+											<Button color="danger" variant="light" onPress={onClose}>
+												Close
+											</Button>
+											<Button color="primary" onPress={onClose}>
+												Action
+											</Button>
+										</ModalFooter>
+									</>
+								)}
+							</ModalContent>
+						</Modal>
 					</div>
 				</div>
 			</Card>
