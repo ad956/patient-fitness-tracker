48b8480d344addc44da467eae6f55d5d7808392e
Anand Suthar <anandsuthar956@gmail.com>
2024-03-10 22:21:33 +0530

    initial

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,13 +1,13 @@
 import { Card } from "@nextui-org/react";
 
 export default function Footer() {
-  return (
-    <footer className="h-3/5 flex flex-col justify-center p-5">
-      <Card isBlurred shadow="lg" radius="lg" className="bg-[#1c1624] h-full">
-        <p className="text-white text-2xl font-semibold">
-          Patient Fitness Tracker
-        </p>
-      </Card>
-    </footer>
-  );
+	return (
+		<footer className="h-3/5 flex flex-col justify-center p-5">
+			<Card isBlurred shadow="lg" radius="lg" className="bg-[#1c1624] h-full">
+				<p className="text-white text-2xl font-semibold">
+					Patient Fitness Tracker
+				</p>
+			</Card>
+		</footer>
+	);
 }
