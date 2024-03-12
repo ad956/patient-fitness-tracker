4d37fe524270d0d22a5a0879811e20ca39aab9ff
Anand Suthar <anandsuthar956@gmail.com>
2024-03-12 18:16:17 +0530

    redirection using redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-"use client";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import LandingPage from "./layouts/LandingPage";
 
 export default function Home() {
-	const router = useRouter();
-
 	const isLogged = true;
-	return isLogged ? router.push("/patient") : <LandingPage />;
+	return isLogged ? redirect("/patient") : <LandingPage />;
 }
