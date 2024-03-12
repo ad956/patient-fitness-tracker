be451b866af660459d778a40cee3ead08577d4f0
Anand Suthar <anandsuthar956@gmail.com>
2024-03-12 15:16:20 +0530

    minor

--- app/(pages)/(footer)/layout.tsx
+++ app/(pages)/(footer)/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/app/components/footer";
-import Navbar from "@/app/components/navbar";
+import Footer from "@/app/layouts/LandingPage/components/footer";
+import Navbar from "@/app/layouts/LandingPage/components/navbar";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
