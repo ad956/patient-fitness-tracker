bd554fb6e8af0121c4eb98993e2262e9c1ce48b3
Anand Suthar <anandsuthar956@gmail.com>
2024-03-13 22:58:54 +0530

    Completed dashboard design

--- app/(pages)/patient/page.tsx
+++ app/(pages)/patient/page.tsx
@@ -1,9 +1,20 @@
-import { Avatar, Card, Divider, Progress, Spacer } from "@nextui-org/react";
+import {
+	Avatar,
+	Card,
+	CircularProgress,
+	Divider,
+	Progress,
+	Select,
+	SelectItem,
+	Spacer,
+	Tooltip,
+} from "@nextui-org/react";
 import React from "react";
+import { WeeklyProgress, HealthConditions } from "./components/Graphs";
 
 export default function Patient() {
 	return (
-		<section className="border- border-violet-700 bg-[#f3f6fd] h-full p-2">
+		<section className="border- border-violet-700 bg-[#f3f6fd] overflow-hidden p-2">
 			<div className="grid grid-cols-5 grid-rows-5 h-full gap-3">
 				<Card className="row-span-3 justify-center items-center">
 					<div className="flex flex-col items-center">
@@ -17,6 +28,7 @@ export default function Patient() {
 
 					<div className="p-5 w-full text-sm font-semibold m-2 text-black/75 flex flex-col gap-2">
 						<p className="self-center">Details</p>
+
 						<Divider className="mx-2 w-full bg-black/10" />
 						<div className="flex flex-row justify-between">
 							<div className="flex flex-col gap-2">
@@ -63,10 +75,54 @@ export default function Patient() {
 						showValueLabel={true}
 					/>
 				</Card>
-				<Card className="col-span-1 ">3</Card>
+				<Card className="col-span-1 flex justify-center gap-5 items-center p-5">
+					<Progress
+						size="sm"
+						radius="sm"
+						color="success"
+						classNames={{
+							base: "max-w-md",
+							track: "drop-shadow-md border border-default",
+							label: "tracking-wider font-bold text-xs",
+							value: "text-foreground/60 text-xs",
+						}}
+						label="Current Treatment"
+						value={10}
+						showValueLabel={true}
+					/>
+					<Tooltip
+						showArrow={true}
+						placement="right-end"
+						content={
+							<div className="px-1 py-2">
+								<div className="text-small font-bold">Tip</div>
+								<div className="text-tiny">Each 10 % equals 1 appointment</div>
+							</div>
+						}
+					>
+						<Progress
+							size="sm"
+							radius="sm"
+							color="warning"
+							classNames={{
+								base: "max-w-md",
+								track: "drop-shadow-md border border-default",
+								label: "tracking-wider font-bold text-xs",
+								value: "text-foreground/60 text-xs",
+							}}
+							label="Pending Appointments"
+							value={0}
+							showValueLabel={true}
+						/>
+					</Tooltip>
+				</Card>
 				<Card className=" row-span-5 col-span-2">5</Card>
-				<Card className="col-span-2 row-span-2">4</Card>
-				<Card className=" row-span-2 col-span-3">6</Card>
+				<Card className="col-span-2 row-span-2 flex flex-row justify-center">
+					<WeeklyProgress />
+				</Card>
+				<Card className=" row-span-2 col-span-3">
+					<HealthConditions />
+				</Card>
 			</div>
 		</section>
 	);
