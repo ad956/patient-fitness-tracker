72ad5e729edce8b7298af2c22cd9539456446371
Anand Suthar <anandsuthar956@gmail.com>
2024-03-13 16:01:42 +0530

    Grid layout utilised

--- app/(pages)/patient/page.tsx
+++ app/(pages)/patient/page.tsx
@@ -1,32 +1,45 @@
-import { Card } from "@nextui-org/react";
+import { Avatar, Card, Divider, Spacer } from "@nextui-org/react";
 import React from "react";
 
 export default function Patient() {
 	return (
-		<section className="bg-[#f3f6fd] h-full flex gap-5 p-2 border2 border-violet-700">
-			<Card shadow="lg" className="h-full flex flex-col w-3/5 p-10">
-				<div className="flex justify-between items-center">
-					<p className="text-lg font-bold">Dashboard</p>
-					<p className="text-sm font-medium">March, 2024</p>
-				</div>
+		<section className="border- border-violet-700 bg-[#f3f6fd] h-full p-2">
+			<div className="grid grid-cols-5 grid-rows-5 h-full gap-3">
+				<Card className="row-span-3 justify-center items-center">
+					<div className="flex flex-col items-center">
+						<Avatar
+							size="lg"
+							src="https://i.pravatar.cc/150?u=a04258114e29026302d"
+						/>
+						<p className="text-sm font-semibold">Anand Suthar</p>
+						<p className="text-xs text-gray-700">+91-8078-65-3427</p>
+					</div>
 
-				<div className="flex">
-					<div className="flex flex-col">
-						<p className="">10</p>
-						<p className="">Pending</p>
+					<div className="p-5 w-full text-sm font-semibold m-2 text-black/75 flex flex-col gap-2">
+						<p className="self-center">Details</p>
+						<Divider className="mx-2 w-full bg-black/10" />
+						<div className="flex flex-row justify-between">
+							<div className="flex flex-col gap-2">
+								<p className="">Age :</p>
+								<p className="">Blood Group :</p>
+								<p className="">Height (f) :</p>
+								<p className="">Weight (Kg) :</p>
+							</div>
+							<div className="flex flex-col items-end gap-2 font-bold text-black">
+								<p className="">27 Year</p>
+								<p className="">O+</p>
+								<p className="">5.6</p>
+								<p className="">60</p>
+							</div>
+						</div>
 					</div>
-					<div className="flex flex-col">
-						<p className="">10</p>
-						<p className="">Completed</p>
-					</div>
-					<div className="flex flex-col">
-						<p className="">10</p>
-						<p className="">Upcoming</p>
-					</div>
-				</div>
-			</Card>
-
-			<div className="h-full w-2/5">ch</div>
+				</Card>
+				<Card className="col-span-1">2</Card>
+				<Card className="col-span-1">3</Card>
+				<Card className=" row-span-5 col-span-2">5</Card>
+				<Card className="col-span-2 row-span-2">4</Card>
+				<Card className=" row-span-2 col-span-3">6</Card>
+			</div>
 		</section>
 	);
 }
