fac3441ba696f8652b98432153338d02064e4e0c
Anand Suthar <anandsuthar956@gmail.com>
2024-03-14 23:50:21 +0530

    Completed

--- app/(pages)/patient/components/CarouselService/index.tsx
+++ app/(pages)/patient/components/CarouselService/index.tsx
@@ -1,87 +1,68 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Image } from "@nextui-org/react";
 
-const images = [
-	"https://picsum.photos/id/1015/1000/600",
-	"https://picsum.photos/id/1018/1000/600",
-	"https://picsum.photos/id/1015/1000/600",
-];
+const images = ["/images/about-us.jpg", "/admin.png", "/hospital.png"];
 
 export default function CarouselService() {
-	const [currentIndex, setCurrentIndex] = useState(0);
+	const [currentImg, setCurrentImg] = useState(0);
+	const [carouselSize, setCarouselSize] = useState({ width: 0, height: 0 });
+	const carouselRef = useRef(null);
+	const [intervalId, setIntervalId] = useState("");
 
-	const nextSlide = () => {
-		setCurrentIndex((currentIndex + 1) % images.length);
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		// biome-ignore lint/style/useConst: <explanation>
+		let elem = carouselRef.current as unknown as HTMLDivElement;
+		// biome-ignore lint/style/useConst: <explanation>
+		let { width, height } = elem.getBoundingClientRect();
+		if (carouselRef.current) {
+			setCarouselSize({
+				width,
+				height,
+			});
+		}
+
+		const incrementImageIndex = () => {
+			setCurrentImg((prev) => (prev + 1) % images.length);
+		};
+
+		const id = setInterval(incrementImageIndex, 3000);
+
+		return () => clearInterval(id);
+	}, [images.length]);
+
+	const handleClick = (index: number) => {
+		clearInterval(intervalId); // Clear automatic sliding interval
+		setCurrentImg(index);
 	};
 
-	const prevSlide = () => {
-		setCurrentIndex((currentIndex - 1 + images.length) % images.length);
-	};
 	return (
-		<div className="flex flex-col justify-evenly items-center w-full h-full">
-			<div className="relative w-full h-64 overflow-hidden">
+		<div className="flex justify-center items-center h-full w-full">
+			<div className="w-full h-full rounded-md overflow-hidden relative">
 				<div
-					className="absolute inset-0 w-full h-full transition-transform duration-500"
-					style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+					ref={carouselRef}
+					style={{
+						left: -currentImg * carouselSize.width,
+					}}
+					className="w-full h-full absolute flex transition-all duration-300"
 				>
-					{images.map((image, index) => (
-						<img
+					{images.map((v, i) => (
+						<div
 							// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-							key={index}
-							src={image}
-							alt=""
-							className="object-cover w-full h-full"
-						/>
+							key={i}
+							className="h-full w-full self-center relative shrink-0"
+						>
+							<Image
+								className="pointer-events-none"
+								alt={`carousel-image-${i}`}
+								src={v}
+							/>
+						</div>
 					))}
 				</div>
 			</div>
-			{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-			<button
-				className="absolute top-0 left-0 z-10 flex items-center justify-center h-full px-4 cursor-pointer group"
-				onClick={prevSlide}
-			>
-				<span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/50">
-					<svg
-						className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-						aria-hidden="true"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						strokeWidth={2}
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M15 19l-7-7 7-7"
-						/>
-					</svg>
-				</span>
-			</button>
-			{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-			<button
-				className="absolute top-0 right-0 z-10 flex items-center justify-center h-full px-4 cursor-pointer group"
-				onClick={nextSlide}
-			>
-				<span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/50">
-					<svg
-						className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180"
-						aria-hidden="true"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						strokeWidth={2}
-					>
-						<path
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							d="M9 5l7 7-7 7"
-						/>
-					</svg>
-				</span>
-			</button>
 		</div>
 	);
 }
