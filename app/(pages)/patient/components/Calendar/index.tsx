7ea52ce9057ce6c7be6ca9ae58f2c98eee02da8d
Anand Suthar <anandsuthar956@gmail.com>
2024-03-14 23:04:51 +0530

    calendar styles added

--- app/(pages)/patient/components/Calendar/index.tsx
+++ app/(pages)/patient/components/Calendar/index.tsx
@@ -1,33 +1,55 @@
 "use client";
 import { format } from "date-fns";
 import React from "react";
-import { DayPicker } from "react-day-picker";
+import { type DateFormatter, DayPicker } from "react-day-picker";
 import styles from "./styles.module.css";
+import { Card } from "@nextui-org/react";
+
 export default function Calendar() {
 	const [selected, setSelected] = React.useState<Date>();
 
-	let footer = <p className="text-center text-gray-500">Please pick a day.</p>;
-	if (selected) {
-		footer = (
-			<p className="text-center text-blue-500">
-				You picked {format(selected, "PP")}.
-			</p>
-		);
-	}
+	const seasonEmoji: Record<string, string> = {
+		winter: "⛄️",
+		spring: "🌸",
+		summer: "🌻",
+		autumn: "🍂",
+	};
 
-	const dayPickerStyle = `${styles.rdp_nav}${styles.day_input} ${styles.day_button} ${styles.dialog_sheet} ${styles.bg_white} ${styles.dark_gray} ${styles.black} ${styles.white} ${styles.bg_near_black} ${styles.bg_near_white} ${styles.rdp} ${styles.ba}`;
+	const getSeason = (month: Date): string => {
+		const monthNumber = month.getMonth();
+		if (monthNumber >= 0 && monthNumber < 3) return "winter";
+		if (monthNumber >= 3 && monthNumber < 6) return "spring";
+		if (monthNumber >= 6 && monthNumber < 9) return "summer";
+		return "autumn";
+	};
+
+	const formatCaption: DateFormatter = (month, options) => {
+		const season = getSeason(month);
+		return (
+			<>
+				<span role="img" aria-label={season}>
+					{seasonEmoji[season]}
+				</span>{" "}
+				{format(month, "LLLL", { locale: options?.locale })}
+			</>
+		);
+	};
+
+	const dayPickerStyle = `h-full w-3/5 ${styles.day_input} ${styles.day_button} ${styles.dialog_sheet} ${styles.bg_white} ${styles.dark_gray} ${styles.black} ${styles.white} ${styles.bg_near_black} ${styles.bg_near_white} ${styles.rdp} ${styles.ba}`;
 	return (
-		<div className="w-4/5 p-5 h-4/5">
+		<Card className="bg-[#111111] w-full h-full flex flex-row justify-around items-center p-5">
 			<DayPicker
 				mode="single"
 				showOutsideDays
-				// onDayClick={}
 				fromYear={2024}
 				selected={selected}
 				onSelect={setSelected}
-				footer={footer}
 				className={dayPickerStyle}
+				formatters={{ formatCaption }}
 			/>
-		</div>
+			<p className="text-white/80 text-md font-medium">
+				{selected ? format(selected, "LLLL dd, yyyy") : "Please pick a day."}
+			</p>
+		</Card>
 	);
 }
