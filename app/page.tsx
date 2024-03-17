2a0482fe19a8bb28c50583514b7a9f3c1e74b8a6
Anand Suthar <anandsuthar956@gmail.com>
2024-03-17 23:45:41 +0530

    using useSelector for user auth check

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import { redirect } from "next/navigation";
 import LandingPage from "./layouts/LandingPage";
+import { useSelector } from "react-redux";
+import { RootState } from "@/lib/store";
 
 export default function Home() {
-	const isLogged = true;
-	return isLogged ? redirect("/patient") : <LandingPage />;
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
+  console.log("isAuth : " + isAuthenticated);
+  return isAuthenticated ? redirect("/patient") : <LandingPage />;
 }
