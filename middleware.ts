bb15bd971e530ebc5d89d687dacd95dcc69cc8f4
Anand Suthar <anandsuthar956@gmail.com>
2024-03-18 23:22:07 +0530

    Added middleware for route protection

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,32 @@
-// import { NextRequest } from "next/server";
-// import { updateSession } from "./lib/authUtils";
+import { NextRequest } from "next/server";
+import { updateSession } from "./lib/authUtils";
 
-// export async function middleware(request: NextRequest) {
-//   return await updateSession(request);
-// }
+export async function middleware(request: NextRequest) {
+  await updateSessionMiddleware(request);
+  redirectMiddleware(request);
+}
+
+async function updateSessionMiddleware(request: NextRequest) {
+  return await updateSession(request);
+}
+
+function redirectMiddleware(request: NextRequest) {
+  const currentUser = request.cookies.get("session")?.value;
+  console.log("currentUser : " + currentUser + " __ " + Date.now());
+
+  if (currentUser && !request.nextUrl.pathname.startsWith("/dashboard")) {
+    return Response.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (!currentUser && !request.nextUrl.pathname.startsWith("/login")) {
+    return Response.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  missing: [
+    { type: "header", key: "next-router-prefetch" },
+    { type: "header", key: "purpose", value: "prefetch" },
+  ],
+};
