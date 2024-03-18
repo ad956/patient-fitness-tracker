8aa3453d8149d5d93761c9b3cf8f55fcf9eee38b
Anand Suthar <anandsuthar956@gmail.com>
2024-03-18 23:24:06 +0530

    minor

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { Provider } from "react-redux";
-import { store } from "@/lib/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <Provider store={store}>
-      <NextUIProvider>{children}</NextUIProvider>{" "}
-    </Provider>
-  );
+  return <NextUIProvider>{children}</NextUIProvider>;
 }
