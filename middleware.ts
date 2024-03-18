aa6d22b447b52e393c0864b73a4a35ef3b4eadc7
Anand Suthar <anandsuthar956@gmail.com>
2024-03-18 20:36:18 +0530

    Added middleware file

---
+++ middleware.ts
@@ -0,0 +1,6 @@
+// import { NextRequest } from "next/server";
+// import { updateSession } from "./lib/authUtils";
+
+// export async function middleware(request: NextRequest) {
+//   return await updateSession(request);
+// }
