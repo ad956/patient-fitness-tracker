fa402f312416bd775fcc47a59829f06c4a9055cb
Anand Suthar <anandsuthar956@gmail.com>
2024-03-19 21:54:38 +0530

    Added new types

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,32 @@
-interface User {
+export type User = {
   id: number;
   name: string;
   username: string;
   email: string;
-}
+  contact: string;
+  profile: string;
+};
+
+export type Patient = User & {
+  physicalDetails: {
+    age: number;
+    blood: string;
+    height: number;
+    weight: number;
+  };
+  progress: {
+    generalHealth: number;
+    waterBalance: number;
+    currentTreatment: number;
+    pendingAppointments: number;
+  };
+  activity: number[];
+  healthConditions: number[];
+  upcomingAppointments: [
+    {
+      day: number;
+      month: string;
+      year: number;
+    }
+  ];
+};
