0997daa74fe7342d9fbc77c21cf24150696f31ca
Anand Suthar <anandsuthar956@gmail.com>
2024-03-19 21:56:31 +0530

    updated

--- app/utils/constants/index.ts
+++ app/utils/constants/index.ts
@@ -43,4 +43,35 @@ const projectFeatures = [
   },
 ];
 
-export { carouselData, projectFeatures };
+const states = [
+  "Andhra Pradesh",
+  "Arunachal Pradesh",
+  "Assam",
+  "Bihar",
+  "Chhattisgarh",
+  "Goa",
+  "Gujarat",
+  "Haryana",
+  "Himachal Pradesh",
+  "Jharkhand",
+  "Karnataka",
+  "Kerala",
+  "Madhya Pradesh",
+  "Maharashtra",
+  "Manipur",
+  "Meghalaya",
+  "Mizoram",
+  "Nagaland",
+  "Odisha",
+  "Punjab",
+  "Rajasthan",
+  "Sikkim",
+  "Tamil Nadu",
+  "Telangana",
+  "Tripura",
+  "Uttar Pradesh",
+  "Uttarakhand",
+  "West Bengal",
+];
+
+export { carouselData, projectFeatures, states };
