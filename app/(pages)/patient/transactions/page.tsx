e5bb49afa8998626521c9e1312c7cf146a2afeb5
Anand Suthar <anandsuthar956@gmail.com>
2024-03-20 22:45:54 +0530

    Transactions page completed

--- app/(pages)/patient/transactions/page.tsx
+++ app/(pages)/patient/transactions/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Payments from "../components/payments";
 
 export default function Transactions() {
-	return <div>Transactions</div>;
+  return (
+    <section className="h-full w-full flex flex-col gap-5 items-center p-5 overflow-hidden">
+      <p className="self-start font-medium text-lg tracking-wider ml-5">
+        Payment History
+      </p>
+      <Payments />
+    </section>
+  );
 }
