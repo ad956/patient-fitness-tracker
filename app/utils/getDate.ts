162886f1c413667f0c324d35d4c3c38a5e013b5c
Anand Suthar <anandsuthar956@gmail.com>
2024-03-21 16:12:22 +0530

    Utility method to get formatted date

---
+++ app/utils/getDate.ts
@@ -0,0 +1,22 @@
+export function getCurrentDateFormatted(): string {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const currentDate = new Date();
+  const day = currentDate.getDate();
+  const month = months[currentDate.getMonth()];
+  const year = currentDate.getFullYear();
+
+  return `${day} ${month} ${year}`;
+}
