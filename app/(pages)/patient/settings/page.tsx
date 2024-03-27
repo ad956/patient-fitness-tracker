d6af6a7d7e1a5e77e3470de8433f7e870c29552b
Anand Suthar <anandsuthar956@gmail.com>
2024-03-27 23:21:38 +0530

    initial

--- app/(pages)/patient/settings/page.tsx
+++ app/(pages)/patient/settings/page.tsx
@@ -4,7 +4,7 @@ import ProfileSettings from "../components/ProfileSettings";
 export default function Settings() {
   return (
     <section className="h-full w-full flex flex-col">
-      <p className="text-default-800 font-medium tracking-wide text-xl ml-5 mt-5">
+      <p className="self-start font-medium text-lg tracking-wider ml-5">
         Settings
       </p>
 
