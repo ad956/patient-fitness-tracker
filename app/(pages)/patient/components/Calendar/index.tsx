82de3d8587b48c1e2fcf06bb9fb4dd515812851b
Anand Suthar <anandsuthar956@gmail.com>
2024-03-29 16:39:22 +0530

    minor

--- app/(pages)/patient/components/Calendar/index.tsx
+++ app/(pages)/patient/components/Calendar/index.tsx
@@ -3,6 +3,7 @@ import { format } from "date-fns";
 import React from "react";
 import { type DateFormatter, DayPicker } from "react-day-picker";
 import {
+  Avatar,
   Card,
   Modal,
   ModalBody,
@@ -11,28 +12,44 @@ import {
   useDisclosure,
 } from "@nextui-org/react";
 import { getFormattedDate } from "@/app/utils/getDate";
+import { LuCalendarClock } from "react-icons/lu";
+import { bookedAppointments } from "@/types";
 
-type upcomingAppointmentProps = {
-  upcomingAppointments: [
-    {
-      date: string;
-      state: string;
-      city: string;
-      hospital: string;
-      disease: string;
-      note: string;
-      approved: string;
-    }
-  ];
-};
+// type upcomingAppointmentProps = {
+//   upcomingAppointments: [
+//     {
+//       date: string;
+//       timing: string;
+//       state: string;
+//       city: string;
+//       hospital: string;
+//       disease: string;
+//       note: string;
+//       approved: string;
+//     }
+//   ];
+// };
 
-export default function Calendar({
-  upcomingAppointments,
-}: upcomingAppointmentProps) {
+export default function Calendar() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+  const upcomingAppointments: bookedAppointments = [
+    {
+      date: "2024-03-30T15:59:59.094Z",
+      timing: "02:25 PM - 03:00 PM",
+      state: "Gujarat",
+      city: "Vadodara",
+      hospital: "Pearl Hospital",
+      disease: "Common Cold",
+      note: "I got common cold from yesterday.",
+      approved: "approved",
+      doctor_id: "660682d54e864f93f957de86",
+    },
+  ];
 
   const [appointmentDetail, setappointmentDetail] = React.useState<{
     date: string;
+    timing: string;
     state: string;
     city: string;
     hospital: string;
@@ -129,28 +146,54 @@ export default function Calendar({
             <ModalHeader className="flex flex-col gap-1 font-bold">
               <p className="text-danger mx-2"> Appointment Details</p>
             </ModalHeader>
-            <ModalBody>
-              <p>
-                <span className="font-bold">Date:</span>
-                {appointmentDetail &&
-                  getFormattedDate(new Date(appointmentDetail.date))}
-              </p>
-              <p>
-                <span className="font-bold">Disease :</span>
-                {appointmentDetail?.disease}
-              </p>
-              <p>
-                <span className="font-bold">Hospital :</span>
-                {appointmentDetail?.hospital}
-              </p>
-              <p>
-                <span className="font-bold">Note :</span>
-                {appointmentDetail?.note}
-              </p>
-              <p>
-                <span className="font-bold">City :</span>
-                {appointmentDetail?.city}
-              </p>
+            <ModalBody className="flex flex-col items-start">
+              <div className="flex flex-row w-full p-2 justify-around items-center ">
+                <Avatar
+                  src="https://i.pravatar.cc/150?u=a04258114e29026708c"
+                  className="w-30 h-30 text-large"
+                />
+                <div className="flex flex-col justify-center items-start">
+                  <div className="mb-2">
+                    <p className="font-bold text-xl">John Doe</p>
+                    <p className="font-bold text-black/80 text-sm">
+                      Sr. Doctor
+                    </p>
+                  </div>
+
+                  <div className="flex flex-row justify-center items-center gap-2">
+                    <LuCalendarClock size={25} className="text-warning" />
+                    <div className="flex flex-col">
+                      <p className="font-bold text-black/80 text-md">
+                        {appointmentDetail &&
+                          getFormattedDate(new Date(appointmentDetail.date))}
+                      </p>
+                      <p className="font-bold text-black/70 text-sm">
+                        {appointmentDetail?.timing}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <table className="border-collapse w-full mt-4">
+                <tbody>
+                  <tr className="border-b border-gray-200">
+                    <td className="font-bold pr-4 py-2">Disease</td>
+                    <td className="py-2">{appointmentDetail?.disease}</td>
+                  </tr>
+                  <tr className="border-b border-gray-200">
+                    <td className="font-bold pr-4 py-2">Hospital</td>
+                    <td className="py-2">{appointmentDetail?.hospital}</td>
+                  </tr>
+                  <tr className="border-b border-gray-200">
+                    <td className="font-bold pr-4 py-2">Note</td>
+                    <td className="py-2">{appointmentDetail?.note}</td>
+                  </tr>
+                  <tr>
+                    <td className="font-bold pr-4 py-2">City</td>
+                    <td className="py-2">{appointmentDetail?.city}</td>
+                  </tr>
+                </tbody>
+              </table>
             </ModalBody>
           </>
         </ModalContent>
