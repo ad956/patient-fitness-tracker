c65ec151329fbf052b1cfffd3208164170120ff7
Anand Suthar <anandsuthar956@gmail.com>
2024-03-31 23:29:55 +0530

    Doctor api added

---
+++ app/api/doctor/route.ts
@@ -0,0 +1,33 @@
+import dbConfig from "@/lib/db";
+
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const email = searchParams.get("email");
+
+    if (!email) {
+      return new Response("Email parameter is missing", {
+        status: 400,
+      });
+    }
+
+    const db = await dbConfig();
+    const doctor_collection = db.collection("doctor");
+    const doctor = await doctor_collection.findOne({ email });
+
+    if (!doctor) {
+      return new Response(JSON.stringify({ error: "doctor not found" }), {
+        status: 404,
+      });
+    }
+
+    return new Response(JSON.stringify(doctor), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error getting appointments:", error);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+}
