cb406dc570d8e27842e231cb641cc12621adda89
Anand Suthar <anandsuthar956@gmail.com>
2024-04-02 23:36:10 +0530

    Getting notifications from server

--- app/(pages)/patient/components/Notifications/index.tsx
+++ app/(pages)/patient/components/Notifications/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { bookedAppointments } from "@/types";
 import {
   Popover,
   PopoverTrigger,
@@ -11,15 +12,49 @@ import {
 import React from "react";
 import { CiBellOn } from "react-icons/ci";
 import { FcCloseUpMode } from "react-icons/fc";
+type upcomingAppointmentsType = {
+  upcomingAppointments: bookedAppointments;
+};
 
-export default function Notifications() {
+export default function Notifications({
+  upcomingAppointments,
+}: upcomingAppointmentsType) {
   const [isOpen, setIsOpen] = React.useState(false);
+
+  //
+  const [appointmentDetail, setappointmentDetail] = React.useState<{
+    date: string;
+    timing: string;
+    state: string;
+    city: string;
+    hospital: string;
+    disease: string;
+    note: string;
+    approved: string;
+    doctor: {
+      name: string;
+      profile: string;
+      specialty: string;
+    };
+  } | null>(null);
+
+  // React.useEffect(() => {
+  //   if (upcomingAppointments && upcomingAppointments.length > 0) {
+  //     const selectedDates = upcomingAppointments
+  //       .filter((appointment) => appointment.approved === "approved")
+  //       .map((approvedAppointment) => new Date(approvedAppointment.date));
+  //     setappointmentDetail(selectedDates);
+  //   } else {
+  //     setappointmentDetail([]);
+  //   }
+  // }, [upcomingAppointments]);
+
   return (
     <Popover
       isOpen={isOpen}
       onOpenChange={(open) => setIsOpen(open)}
       showArrow
-      placement="bottom-start"
+      placement="bottom"
       size="lg"
       offset={10}
     >
@@ -29,25 +64,42 @@ export default function Notifications() {
         </Button>
       </PopoverTrigger>
       <PopoverContent>
-        <div className="px-1 py-2">
-          <div className="text-small font-bold">Notifications</div>
-          <div className="text-tiny">
-            <Listbox aria-label="Actions">
-              <ListboxItem key="" className="flex flex-row gap-5">
-                <FcCloseUpMode /> This is a notification
-              </ListboxItem>
-              <ListboxItem key="" className="flex flex-row gap-5">
-                <FcCloseUpMode /> This is a notification
-              </ListboxItem>
-              <ListboxItem key="" className="flex flex-row gap-5">
-                <FcCloseUpMode /> This is a notification
-              </ListboxItem>
-              <ListboxItem key="" className="flex flex-row gap-5">
-                <FcCloseUpMode /> This is a notification
-              </ListboxItem>
-            </Listbox>
-          </div>
-        </div>
+        <div className="text-md font-bold self-start my-2">Notifications</div>
+        <Listbox aria-label="Actions">
+          <ListboxItem key="">
+            <div className="flex flex-row justify-center items-center gap-5">
+              <FcCloseUpMode size={25} />
+              <p className="text-sm max-w-xs text-wrap">
+                Your appointment request has been accepted! Join us on{" "}
+                <span className="text-danger">21 Apr 2024</span> at{" "}
+                <span className="text-danger">Apollo Hospitals</span> for your
+                consultation.
+              </p>
+            </div>
+          </ListboxItem>
+          <ListboxItem key="">
+            <div className="flex flex-row justify-center items-center gap-5">
+              <FcCloseUpMode size={25} />
+              <p className="text-sm max-w-xs text-wrap">
+                Your appointment request has been accepted! Join us on{" "}
+                <span className="text-danger">21 Apr 2024</span> at{" "}
+                <span className="text-danger">Apollo Hospitals</span> for your
+                consultation.
+              </p>
+            </div>
+          </ListboxItem>
+          <ListboxItem key="">
+            <div className="flex flex-row justify-center items-center gap-5">
+              <FcCloseUpMode size={25} />
+              <p className="text-sm max-w-xs text-wrap">
+                Your appointment request has been accepted! Join us on{" "}
+                <span className="text-danger">21 Apr 2024</span> at{" "}
+                <span className="text-danger">Apollo Hospitals</span> for your
+                consultation.
+              </p>
+            </div>
+          </ListboxItem>
+        </Listbox>
       </PopoverContent>
     </Popover>
   );
