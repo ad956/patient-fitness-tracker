bef6c0a4e22b44391d7ce03115fddf7c69e4b035
Anand Suthar <anandsuthar956@gmail.com>
2024-04-04 23:59:46 +0530

    removed unnecessary console logs

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,5 @@ import { getSession } from "@/lib/sessions/sessionUtils";
 export default async function Home() {
   const isAuthenticated = await getSession();
   const userRole = isAuthenticated?.user.role;
-  console.log("isAuth user : " + userRole);
   return isAuthenticated ? redirect(`/${userRole}`) : <LandingPage />;
 }
