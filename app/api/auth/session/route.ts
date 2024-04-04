5f9f0e7108c085730dc7ba2e46a0b4ba6a71ecc4
Anand Suthar <anandsuthar956@gmail.com>
2024-04-05 00:02:12 +0530

    completion of session apis

--- app/api/auth/session/route.ts
+++ app/api/auth/session/route.ts
@@ -7,27 +7,24 @@ export async function GET() {
     const res = await getSession();
 
     if (res) {
-      console.log("session on ways");
       return Response.json(
-        { message: "Session get successfully", res },
+        { message: "Session is available", res },
         { status: 200 }
       );
     }
-
     return Response.json(
       { message: "Session isn't available", res },
       { status: 401 }
     );
   } catch (error) {
-    console.error("Error setting session:", error);
-    return Response.json({ error: "Error setting session" }, { status: 500 });
+    console.error("Error getting session:", error);
+    return Response.json({ error: "Error getting session" }, { status: 500 });
   }
 }
 
 // setting session
 export async function POST(req: Request) {
   const { email, role } = await req.json();
-  console.log("AT SERVER : " + email + " " + role);
   try {
     // from sessionUtils
     await setSession(email, role);
