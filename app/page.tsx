8ebdac3a1c7db0715d652e520cbce773686453fc
Anand Suthar <anandsuthar956@gmail.com>
2024-04-04 23:49:41 +0530

    removed unnecessary items

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-// "use client";
-
 import { redirect } from "next/navigation";
 import LandingPage from "./layouts/LandingPage";
-import { getSession } from "@sessions/sessionUtils";
+import { getSession } from "@/lib/sessions/sessionUtils";
 
 export default async function Home() {
-  const isAuthenticated = getSession();
+  const isAuthenticated = await getSession();
   const user = await isAuthenticated;
-  // console.log("isAuth user : " + user);
-  // return user ? redirect("/patient") : <LandingPage />;
-  return <LandingPage />;
+  console.log("isAuth user : " + user);
+  return user ? redirect("/patient") : <LandingPage />;
 }
