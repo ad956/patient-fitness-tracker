c1fd18ebfb9fa94b6774f5dc8a120dddb2a303e9
Anand Suthar <anandsuthar956@gmail.com>
2024-04-04 15:36:39 +0530

    login action issue resolved

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,28 +1,24 @@
 "use server";
 
 import { login, logout } from "@/lib/authUtils";
-import { User } from "@/types";
 import { redirect } from "next/navigation";
 
 export async function loginAction(formData: FormData) {
   const email = formData.get("email");
   const password = formData.get("password");
   const role = formData.get("role");
+
   try {
     const response = await fetch("http://localhost:3000/api/auth", {
       method: "POST",
       body: JSON.stringify({ email, password, role }),
     });
 
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.log("kon vetu login " + errorData);
-
-      return Error(errorData.error || "Failed to login");
-    }
-
     const userData = await response.json();
-    return userData;
+
+    if (!response.ok) {
+      return { msg: userData.error, unauthorized: true };
+    } else return userData;
   } catch (error) {
     console.error("Login failed:", error);
   }
