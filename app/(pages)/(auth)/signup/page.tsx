2a92f746efd7206b056540b7b4a728fa0c16c0b7
Anand Suthar <anandsuthar956@gmail.com>
2024-04-05 16:51:45 +0530

    completion of signup page

--- app/(pages)/(auth)/signup/page.tsx
+++ app/(pages)/(auth)/signup/page.tsx
@@ -5,196 +5,335 @@ import { AiTwotoneEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { carouselData } from "@/app/utils/constants";
 import Carousel from "@/app/components/carousel";
 import Link from "next/link";
-import { Button } from "@nextui-org/react";
+import { Button, Input, Select, SelectItem } from "@nextui-org/react";
+import toast, { Toaster } from "react-hot-toast";
+import { MdAlternateEmail, MdOutlineKey } from "react-icons/md";
+import { FaRegUserCircle, FaUserCircle } from "react-icons/fa";
+import { PiUserCircleDuotone } from "react-icons/pi";
+import { BsCalendar2Date } from "react-icons/bs";
 
 export default function Signup() {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [emailError, setEmailError] = useState(null || String);
-	const [password, setPassword] = useState("");
-	const [passwordError, setPasswordError] = useState(null || String);
-	const [confirmPassword, setconfirmPassword] = useState("");
-	const [confirmPasswordError, setConfirmPasswordError] = useState("");
-	const [isVisible, setIsVisible] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(null || String);
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(null || String);
+  const [confirmPassword, setconfirmPassword] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [isVisible, setIsVisible] = useState(false);
 
-	function handleNameChange(e: ChangeEvent<HTMLInputElement>) {
-		setName(e.target.value);
-		name;
-	}
-	function handleEmailChange(e: ChangeEvent<HTMLInputElement>) {
-		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-		const isValidEmail = emailRegex.test(e.target.value);
-		setEmailError(isValidEmail ? "" : "Please enter a valid email address");
-		setEmail(e.target.value);
-	}
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordRef = useRef<HTMLInputElement>(null);
 
-	function handlePasswordChange(e: ChangeEvent<HTMLInputElement>) {
-		const passwordRegex =
-			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-		const isValidPassword = passwordRegex.test(e.target.value);
-		setPasswordError(
-			isValidPassword
-				? ""
-				: "Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one special character (@, $, !, %, *, ?, &)",
-		);
-		setPassword(e.target.value);
-	}
-	function handleConfirmPasswordChange(e: ChangeEvent<HTMLInputElement>) {
-		const passwordRegex =
-			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-		const isValidPassword = passwordRegex.test(e.target.value);
-		setConfirmPasswordError(
-			isValidPassword
-				? ""
-				: "Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, one number, and one special character (@, $, !, %, *, ?, &)",
-		);
-		setconfirmPassword(e.target.value);
-	}
+  const toggleVisibility = () => setIsVisible(!isVisible);
 
-	const ref = useRef<HTMLInputElement>(null);
+  function handleNameChange(e: ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+    name;
+  }
 
-	const handleClick = () => {
-		setIsVisible(!isVisible);
-		if (ref.current) {
-			ref.current.type = isVisible ? "password" : "text";
-		}
-	};
+  function handleEmailChange(e: ChangeEvent<HTMLInputElement>) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(e.target.value);
 
-	function handleFormSubmit() {}
+    setEmailError(isValidEmail ? "" : "Please enter a valid email address");
+    setEmail(e.target.value);
+  }
 
-	return (
-		<div
-			className={
-				"bg-[#eef1f8] h-screen flex flex-col justify-center lg:flex-row  lg:justify-around"
-			}
-		>
-			{/* // right part only visible from lg */}
-			<div className="hidden lg:w-2/5 m-2 bg-[#161313] rounded-t-2xl rounded-br-2xl  rounded-bl-[40px]  lg:flex lg:flex-col lg:justify-center lg:items-center">
-				<Carousel data={carouselData} />
-			</div>
+  function handlePasswordChange(e: ChangeEvent<HTMLInputElement>) {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const isValidPassword = passwordRegex.test(e.target.value);
+    const missingComponents = [];
 
-			{/* left part */}
-			<div className="m-4 lg:w-1/4 flex flex-col gap-8 self-center">
-				<div className="flex justify-center items-center ">
-					<Image src="patient.svg" height="50" width="50" alt="brand-logo" />
-					<h2 className="ml-2 font-bold text-lg">Patient Fitness Tracker</h2>
-				</div>
-				<div className="">
-					<h3 className="text-3xl font-bold tracking-wider">Welcome</h3>
-					<p className="text-[13px] text-gray-500">
-						Please enter your details below
-					</p>
-				</div>
+    if (!/[a-z]/.test(e.target.value)) {
+      missingComponents.push("at least one lowercase letter");
+    }
 
-				<form className="flex flex-col" onSubmit={handleFormSubmit}>
-					<input
-						type="text"
-						placeholder="Name"
-						value={name}
-						autoComplete="username"
-						className="bg-[#fcfdfe] w-full m-2 p-2 rounded-lg border-2 border-gray-300"
-						onChange={handleNameChange}
-					/>
-					<input
-						type="text"
-						placeholder="Email"
-						value={email}
-						autoComplete="username"
-						className="bg-[#fcfdfe] w-full m-2 p-2 rounded-lg border-2 border-gray-300"
-						onChange={handleEmailChange}
-					/>
-					{emailError && <p className="text-red-700 text-sm">{emailError}</p>}
-					<div className="relative">
-						<input
-							type={isVisible ? "text" : "password"}
-							ref={ref}
-							placeholder="Password"
-							value={password}
-							autoComplete="current-password"
-							className="bg-[#fcfdfe] border-2 border-gray-300  m-2 p-2 w-full rounded-lg"
-							onChange={handlePasswordChange}
-						/>
-						<Link href="#">
-							<Button
-								onClick={handleClick}
-								className="absolute inset-y-0 right-0 flex items-center px-3"
-							>
-								{isVisible ? (
-									<AiOutlineEyeInvisible style={{ fontSize: "20px" }} />
-								) : (
-									<AiTwotoneEye style={{ fontSize: "20px" }} />
-								)}
-							</Button>
-						</Link>
-					</div>
-					{passwordError && (
-						<p className="text-red-700 text-sm">{passwordError}</p>
-					)}
-					<div className="relative">
-						<input
-							type={isVisible ? "text" : "password"}
-							ref={ref}
-							placeholder="Confirm password"
-							value={confirmPassword}
-							autoComplete="current-password"
-							className="bg-[#fcfdfe] border-2 border-gray-300  m-2 p-2 w-full rounded-lg"
-							onChange={handleConfirmPasswordChange}
-						/>
-						<Link href="#">
-							<Button
-								onClick={handleClick}
-								className="absolute inset-y-0 right-0 flex items-center px-3"
-							>
-								{isVisible ? (
-									<AiOutlineEyeInvisible style={{ fontSize: "20px" }} />
-								) : (
-									<AiTwotoneEye style={{ fontSize: "20px" }} />
-								)}
-							</Button>
-						</Link>
-					</div>
-					{confirmPasswordError && (
-						<p className="text-red-700 text-sm">{confirmPasswordError}</p>
-					)}
+    if (!/[A-Z]/.test(e.target.value)) {
+      missingComponents.push("at least one uppercase letter");
+    }
 
-					<select
-						name=""
-						id=""
-						className="bg-[#fcfdfe] text-[#9ca3bc] border-2 border-gray-300  m-2 p-2 w-full rounded-lg"
-					>
-						<option className="" value="NA">
-							Select Role
-						</option>
-						<option value="Hospital">Hospital</option>
-						<option value="Hospital">Patient</option>
-						<option value="Doctor">Doctor</option>
-						<option value="Receptionist">Receptionist</option>
-					</select>
-					<button
-						type="submit"
-						className="text-white text-lg rounded-lg bg-[#0d0909] h-12 ml-4 my-2"
-					>
-						Sign up
-					</button>
-				</form>
+    if (!/[0-9]/.test(e.target.value)) {
+      missingComponents.push("at least one number");
+    }
 
-				<div className="flex justify-center gap-5 items-center">
-					<div className="h-[1px] w-28 bg-gray-500" />
-					<div className="text-sm">or continue</div>
-					<div className="h-[1px] w-28 bg-gray-500" />
-				</div>
+    if (!/[@$!%*?&]/.test(e.target.value)) {
+      missingComponents.push(
+        "at least one special character (@, $, !, %, *, ?, &)"
+      );
+    }
 
-				<Button className="flex justify-center items-center gap-2 text-sm font-thin border-2 border-black rounded-lg h-12 mx-10">
-					<Image src="google.svg" height="30" width="30" alt="google-sign-in" />{" "}
-					Sign up with Google
-				</Button>
-				<p className="text-gray-500 text-sm text-center">
-					Have an account?{" "}
-					<Link href="/login" className="text-black">
-						Sign In
-					</Link>
-				</p>
-			</div>
-		</div>
-	);
+    setPasswordError(
+      isValidPassword
+        ? ""
+        : missingComponents.length > 0
+        ? `Password must contain at least 8 characters, and ${missingComponents.join(
+            " and "
+          )}.`
+        : "Password is too short. It must be at least 8 characters long."
+    );
+    setPassword(e.target.value);
+  }
+
+  function handleConfirmPasswordChange(e: ChangeEvent<HTMLInputElement>) {
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const isValidPassword = passwordRegex.test(e.target.value);
+    const missingComponents = [];
+
+    if (!/[a-z]/.test(e.target.value)) {
+      missingComponents.push("at least one lowercase letter");
+    }
+
+    if (!/[A-Z]/.test(e.target.value)) {
+      missingComponents.push("at least one uppercase letter");
+    }
+
+    if (!/[0-9]/.test(e.target.value)) {
+      missingComponents.push("at least one number");
+    }
+
+    if (!/[@$!%*?&]/.test(e.target.value)) {
+      missingComponents.push(
+        "at least one special character (@, $, !, %, *, ?, &)"
+      );
+    }
+
+    setConfirmPasswordError(
+      isValidPassword
+        ? ""
+        : missingComponents.length > 0
+        ? `Password must contain at least 8 characters, and ${missingComponents.join(
+            " and "
+          )}.`
+        : "Password is too short. It must be at least 8 characters long."
+    );
+    setconfirmPassword(e.target.value);
+  }
+
+  function handleFormSubmit() {}
+
+  const roles = [
+    { label: "Patient", value: "patient" },
+    { label: "Hospital", value: "hospital" },
+    { label: "Receptionist", value: "receptionist" },
+    { label: "Doctor", value: "doctor" },
+  ];
+  const showToast = (inputRef: React.RefObject<HTMLInputElement>) => {
+    if (inputRef.current?.name === "email") {
+      if (emailError) {
+        toast.error(emailError, {
+          position: "bottom-center",
+        });
+      }
+    }
+    if (
+      inputRef.current?.name === "password" ||
+      inputRef.current?.name === "confirmpassword"
+    ) {
+      if (passwordError) {
+        toast.error(passwordError, {
+          position: "bottom-center",
+        });
+      }
+      if (confirmPasswordError) {
+        toast.error(confirmPasswordError, {
+          position: "bottom-center",
+        });
+      }
+    }
+  };
+  return (
+    <div
+      className={
+        "bg-[#eef1f8]/20 h-screen flex flex-col justify-center lg:flex-row  lg:justify-around"
+      }
+    >
+      {/* // right part only visible from lg */}
+      <div className="hidden lg:w-2/5 m-2 bg-[#161313] rounded-t-2xl rounded-br-2xl  rounded-bl-[40px]  lg:flex lg:flex-col lg:justify-center lg:items-center">
+        <Carousel data={carouselData} />
+      </div>
+
+      {/* left part */}
+      <div className="m-4 lg:w-2/5 flex flex-col gap-8 self-center">
+        <div className="flex justify-center items-center ">
+          <Image src="patient.svg" height="50" width="50" alt="brand-logo" />
+          <h2 className="ml-2 font-bold text-lg">Patient Fitness Tracker</h2>
+        </div>
+        <div className="">
+          <h3 className="text-3xl font-bold tracking-wider">Welcome</h3>
+          <p className="text-[13px] text-gray-500">
+            Please enter your details below
+          </p>
+        </div>
+
+        <form className="flex flex-col" onSubmit={handleFormSubmit}>
+          <div className="flex flex-row">
+            <Input
+              type="text"
+              variant="bordered"
+              placeholder="Name"
+              startContent={<FaRegUserCircle size={20} />}
+              size="lg"
+              value={name}
+              autoComplete="username"
+              className="mx-2 my-1"
+              onChange={handleNameChange}
+              ref={emailRef}
+              onBlur={() => showToast(emailRef)}
+            />
+            <Input
+              type="text"
+              variant="bordered"
+              placeholder="Username"
+              startContent={<PiUserCircleDuotone size={25} />}
+              size="lg"
+              value={name}
+              autoComplete="username"
+              className="mx-2 my-1"
+              onChange={handleNameChange}
+              ref={emailRef}
+              onBlur={() => showToast(emailRef)}
+            />
+          </div>
+          <Input
+            type="email"
+            variant="bordered"
+            placeholder="you@example.com"
+            startContent={<MdAlternateEmail size={20} />}
+            size="lg"
+            value={email}
+            autoComplete="username"
+            className="mx-2 my-1"
+            onChange={handleEmailChange}
+            ref={emailRef}
+            onBlur={() => showToast(emailRef)}
+          />
+          <Input
+            name="password"
+            variant="bordered"
+            size="lg"
+            placeholder="Password"
+            startContent={<MdOutlineKey />}
+            value={password}
+            onChange={handlePasswordChange}
+            className="mx-2 my-1"
+            endContent={
+              <button
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
+              >
+                {isVisible ? (
+                  <AiOutlineEyeInvisible className="text-2xl text-default-400 pointer-events-none" />
+                ) : (
+                  <AiTwotoneEye className="text-2xl text-default-400 pointer-events-none" />
+                )}
+              </button>
+            }
+            type={isVisible ? "text" : "password"}
+            ref={passwordRef}
+            onBlur={() => showToast(passwordRef)}
+          />
+          <Input
+            name="confirmpassword"
+            variant="bordered"
+            size="lg"
+            placeholder="Confirm Password"
+            startContent={<MdOutlineKey />}
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            className="mx-2 my-1"
+            endContent={
+              <button
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
+              >
+                {isVisible ? (
+                  <AiOutlineEyeInvisible className="text-2xl text-default-400 pointer-events-none" />
+                ) : (
+                  <AiTwotoneEye className="text-2xl text-default-400 pointer-events-none" />
+                )}
+              </button>
+            }
+            type={isVisible ? "text" : "password"}
+            ref={confirmPasswordRef}
+            onBlur={() => showToast(confirmPasswordRef)}
+          />
+
+          <div className="flex flex-row justify-center items-center">
+            <Select
+              name="role"
+              items={roles}
+              variant="bordered"
+              size="sm"
+              label="Gender"
+              placeholder="Select your gender"
+              className="mx-2 my-2"
+            >
+              {(roles) => (
+                <SelectItem key={roles.value}>{roles.label}</SelectItem>
+              )}
+            </Select>
+            <Input
+              name="dob"
+              type="date"
+              variant="bordered"
+              size="lg"
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+              className="mx-2 my-1"
+              classNames={{
+                inputWrapper: "rounded-lg text-gray-500",
+                input: "text-sm",
+              }}
+              //   ref={confirmPasswordRef}
+              //   onBlur={() => showToast(confirmPasswordRef)}
+            />
+          </div>
+
+          <Select
+            name="role"
+            items={roles}
+            variant="bordered"
+            size="md"
+            label="Role"
+            placeholder="Select who you are"
+            className="mx-2 my-2"
+          >
+            {(roles) => (
+              <SelectItem key={roles.value}>{roles.label}</SelectItem>
+            )}
+          </Select>
+          <button
+            type="submit"
+            className="text-white text-lg rounded-lg bg-[#0d0909] h-12 ml-4 my-2"
+          >
+            Sign up
+          </button>
+        </form>
+
+        {/* <div className="flex justify-center gap-5 items-center">
+          <div className="h-[1px] w-28 bg-gray-500" />
+          <div className="text-sm">or continue</div>
+          <div className="h-[1px] w-28 bg-gray-500" />
+        </div>
+
+        <Button className="flex justify-center items-center gap-2 text-sm font-thin border-2 border-black rounded-lg h-12 mx-10">
+          <Image src="google.svg" height="30" width="30" alt="google-sign-in" />{" "}
+          Sign up with Google
+        </Button> */}
+        <p className="text-gray-500 text-sm text-center">
+          Have an account?{" "}
+          <Link href="/login" className="text-black">
+            Sign In
+          </Link>
+        </p>
+      </div>
+      <Toaster />
+    </div>
+  );
 }
