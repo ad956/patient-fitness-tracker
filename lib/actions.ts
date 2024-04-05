6b310d36afb55739784fc8b8bd3b5a1663d1ed16
Anand Suthar <anandsuthar956@gmail.com>
2024-04-05 23:50:28 +0530

    Added signup action

--- lib/actions.ts
+++ lib/actions.ts
@@ -24,6 +24,27 @@ export async function loginAction(formData: FormData) {
   }
 }
 
+export async function signupAction(formData: FormData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const role = formData.get("role");
+
+  try {
+    const response = await fetch("http://localhost:3000/api/auth", {
+      method: "POST",
+      body: JSON.stringify({ email, password, role }),
+    });
+
+    const userData = await response.json();
+
+    if (!response.ok) {
+      return { msg: userData.error, unauthorized: true };
+    } else return userData;
+  } catch (error) {
+    console.error("Login failed:", error);
+  }
+}
+
 export async function logoutAction() {
   await logout();
   redirect("/");
