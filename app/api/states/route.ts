97e4d9aaf61445542a1e96d6ee566c95635c4866
Anand Suthar <anandsuthar956@gmail.com>
2024-04-07 18:33:03 +0530

    Added state api to get all states

---
+++ app/api/states/route.ts
@@ -0,0 +1,24 @@
+import dbConfig from "@/lib/db";
+
+export async function GET(req: Request) {
+  try {
+    const db = await dbConfig();
+    const collection = db.collection("citystate_hospitals");
+
+    const projection = { _id: 0 };
+    const cursor = await collection.find({}, { projection });
+
+    const states = await cursor.toArray();
+
+    if (!states || states.length === 0) {
+      return new Response("States not found", { status: 404 });
+    }
+
+    const stateNames = states.map((state) => Object.keys(state)[0]);
+
+    return new Response(JSON.stringify(stateNames), { status: 200 });
+  } catch (error) {
+    console.error("Error fetching state data:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
