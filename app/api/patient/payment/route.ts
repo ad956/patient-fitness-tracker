e63326732434f1dd98473539420d6534f6569f3b
Anand Suthar <anandsuthar956@gmail.com>
2024-04-07 23:09:36 +0530

    Completed api for pyament history

--- app/api/patient/payment/route.ts
+++ app/api/patient/payment/route.ts
@@ -1,19 +1,70 @@
+import { ObjectId } from "mongodb"; // Import ObjectId from mongodb library
 import dbConfig from "@/lib/db";
+import { getSession } from "@sessions/sessionUtils";
+
+interface Transaction {
+  _id: ObjectId;
+  hospital: ObjectId;
+}
+
+interface Hospital {
+  _id: ObjectId;
+  firstname: string;
+  lastname: string;
+  profile: string;
+}
 
 export async function GET() {
   try {
+    const userSession = await getSession();
+
+    if (!userSession)
+      return Response.json(
+        { error: "Patient session not found" },
+        { status: 401 }
+      );
+
+    const email = userSession?.user.email;
+
     const db = await dbConfig();
-    const collection = db.collection("patient");
+    const patientCollection = db.collection("patient");
 
-    const patient = await collection.findOne({ id: 1 });
-
+    const patient = await patientCollection.findOne({ email });
     if (!patient) {
       return Response.json({ error: "Patient not found" }, { status: 404 });
     }
 
-    const paymentData = patient.payments;
+    const transactionsCollection = db.collection<Transaction>("transactions"); // Specify Transaction type
+    const transactionProjection = { _id: 0, transaction_id: 0, patient: 0 };
+    const transactions = await transactionsCollection
+      .find({ patient: patient._id })
+      .toArray();
 
-    return Response.json(paymentData);
+    const hospitalIds = transactions.map((transaction) => transaction.hospital);
+
+    const hospitalCollection = db.collection<Hospital>("hospital"); // Specify Hospital type
+
+    const hospitals = await hospitalCollection
+      .find({ _id: { $in: hospitalIds } })
+      .toArray();
+
+    const hospitalMap: Record<string, Hospital> = {};
+
+    hospitals.forEach((hospital) => {
+      hospitalMap[hospital._id.toHexString()] = hospital;
+    });
+
+    const formattedTransactions = transactions.map((transaction) => ({
+      ...transaction,
+      hospital: {
+        name: `${hospitalMap[transaction.hospital.toHexString()].firstname} ${
+          hospitalMap[transaction.hospital.toHexString()].lastname
+        }`,
+        profile: hospitalMap[transaction.hospital.toHexString()].profile,
+      },
+    }));
+
+    return Response.json(formattedTransactions);
   } catch (error) {
     console.error("Error fetching payment data:", error);
     return Response.json({ error: "Internal Server Error" }, { status: 500 });
