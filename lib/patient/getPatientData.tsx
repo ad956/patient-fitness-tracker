68231361846337396711e20a0a134e5751b6c639
Anand Suthar <anandsuthar956@gmail.com>
2024-04-07 12:38:20 +0530

    Updated to bget patient data from session

--- lib/patient/getPatientData.tsx
+++ lib/patient/getPatientData.tsx
@@ -1,24 +1,13 @@
-import { getAccessToken } from "../sessions/sessionUtils";
-
 export async function getPatientData() {
-  const token = await getAccessToken();
-  console.log("giving  token :" + token);
-
-  if (!token) {
-    console.log("Session expired");
-    return;
-  }
-
   const res = await fetch(`http://localhost:3000/api/patient`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
     next: { revalidate: 10 },
   });
 
-  if (!res.ok) throw new Error("Fetching Single User failed");
+  if (!res.ok) {
+    console.error(`Error fetching patient data: ${res.statusText}`);
+    throw new Error("Failed to fetch patient data");
+  }
 
-  // used when working with dynamicParams and notFound() from next/navigation
-  // if (!res.ok) return undefined;
-  return res.json();
+  const patientData = await res.json();
+  return patientData;
 }
