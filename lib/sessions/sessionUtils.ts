3d3abc79d0a38620c1d544a1b8af2e8d257b0d57
Anand Suthar <anandsuthar956@gmail.com>
2024-04-08 22:04:14 +0530

    Added updatesession method

--- lib/sessions/sessionUtils.ts
+++ lib/sessions/sessionUtils.ts
@@ -49,7 +49,7 @@ export async function updateSession(request: NextRequest) {
 
   // Refresh the session so it doesn't expire
   const parsed = await decrypt(session);
-  parsed.expires = new Date(Date.now() + 10 * 1000);
+  parsed.expires = new Date(Date.now() + 30 * 60 * 1000);
   const res = NextResponse.next();
   res.cookies.set({
     name: "session",
