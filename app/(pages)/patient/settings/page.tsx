4d3d9b78d59f8eb2b88bda4f283b7968bb9d9d1b
Anand Suthar <anandsuthar956@gmail.com>
2024-04-12 17:26:24 +0530

    Moved Proflie settings tittle

--- app/(pages)/patient/settings/page.tsx
+++ app/(pages)/patient/settings/page.tsx
@@ -3,9 +3,6 @@ import ProfileSettings from "../components/ProfileSettings";
 export default function Settings() {
   return (
     <section className="h-full w-full flex flex-col">
-      {/* <p className="self-start font-medium text-lg tracking-wider ml-5">
-        Settings
-      </p> */}
       <ProfileSettings />
     </section>
   );
