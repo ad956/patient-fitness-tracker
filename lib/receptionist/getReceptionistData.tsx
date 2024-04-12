5143a90381297717a9d115260b43ff4ad2a0748d
Anand Suthar <anandsuthar956@gmail.com>
2024-04-12 12:34:17 +0530

    Completed the method with Auth headers

--- lib/receptionist/getReceptionistData.tsx
+++ lib/receptionist/getReceptionistData.tsx
@@ -1,17 +1,23 @@
+import { getSessionToken } from "@sessions/sessionUtils";
+
 export async function getReceptionistData() {
+  const session = await getSessionToken();
+
+  const headers = {
+    Authorization: `Bearer ${session}`,
+  };
+
   const res = await fetch(`http://localhost:3000/api/receptionist`, {
-    // cache: "no-cache",
+    headers,
     next: { revalidate: 10 },
   });
 
   if (!res.ok) {
     console.error(`Error fetching receptionist data: ${res.status}`);
-    // throw new Error("Failed to fetch receptionist data");
+    throw new Error("Failed to fetch receptionist data");
   }
 
   const receptionistData = await res.json();
-
-  console.log(receptionistData);
 
   return receptionistData;
 }
