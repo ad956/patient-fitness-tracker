6d1e8956eb3ab3180ef0c1ca544035887f6c3620
Anand Suthar <anandsuthar956@gmail.com>
2024-04-12 23:02:04 +0530

    added daily count

--- app/utils/constants/index.ts
+++ app/utils/constants/index.ts
@@ -46,37 +46,6 @@ const projectFeatures = [
   },
 ];
 
-const states = [
-  "Andhra Pradesh",
-  "Arunachal Pradesh",
-  "Assam",
-  "Bihar",
-  "Chhattisgarh",
-  "Goa",
-  "Gujarat",
-  "Haryana",
-  "Himachal Pradesh",
-  "Jharkhand",
-  "Karnataka",
-  "Kerala",
-  "Madhya Pradesh",
-  "Maharashtra",
-  "Manipur",
-  "Meghalaya",
-  "Mizoram",
-  "Nagaland",
-  "Odisha",
-  "Punjab",
-  "Rajasthan",
-  "Sikkim",
-  "Tamil Nadu",
-  "Telangana",
-  "Tripura",
-  "Uttar Pradesh",
-  "Uttarakhand",
-  "West Bengal",
-];
-
 const roles = [
   { label: "Patient", value: "patient" },
   { label: "Hospital", value: "hospital" },
@@ -147,6 +116,11 @@ const doctoradditionalDetails = {
 
 const receptionistadditionalDetails = {
   ...commonadditionalDetails,
+  dailyCount: {
+    approved: 0,
+    pending: 0,
+    waiting: 0,
+  },
 };
 
 export {
@@ -154,7 +128,6 @@ export {
   SERVER_URL,
   carouselData,
   projectFeatures,
-  states,
   roles,
   patientadditionalDetails,
   hospitaladditionalDetails,
