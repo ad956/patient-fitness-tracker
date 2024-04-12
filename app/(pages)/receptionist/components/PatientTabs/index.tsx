a601cb79c91c4fd2356cbcba44661c2741674a3b
Anand Suthar <anandsuthar956@gmail.com>
2024-04-13 01:35:58 +0530

    completed pending patients

--- app/(pages)/receptionist/components/PatientTabs/index.tsx
+++ app/(pages)/receptionist/components/PatientTabs/index.tsx
@@ -1,249 +1,38 @@
 "use client";
 
-import {
-  Avatar,
-  Chip,
-  Listbox,
-  ListboxItem,
-  Tab,
-  Tabs,
-} from "@nextui-org/react";
 import React from "react";
 import { GiLotus } from "react-icons/gi";
+import {
+  Listbox,
+  ListboxItem,
+  Tabs,
+  Tab,
+  Chip,
+  Avatar,
+} from "@nextui-org/react";
+import { PatientDetails } from "@/types";
 
-export default function PatientTabs() {
+interface PatientTabsProps {
+  pendingAppointments: { patientDetails: PatientDetails[] };
+}
+
+export default function PatientTabs({ pendingAppointments }: PatientTabsProps) {
   const tabsData = [
     {
       key: "patient-pending",
       title: "Pending Patients",
-      count: 7,
-      data: "Integrate with wearable devices and health tracking apps to collect and analyze patient health data, providing insights to both patients and healthcare providers for proactive management of health conditions.",
+      count: pendingAppointments.patientDetails.length,
     },
 
     {
       key: "approved",
       title: "Approved Patients",
-      count: 2,
-      data: "Enable patients to schedule appointments online and receive reminders via email or SMS, reducing no-show rates and improving appointment adherence.",
+      count: pendingAppointments.patientDetails.length,
     },
     {
       key: "patient-waiting",
       title: "Waiting Patients",
-      count: 5,
-      data: "Efficiently manage patient records, including medical history, appointments, and treatment plans, ensuring accurate and up-to-date information is accessible to healthcare providers as needed.",
-    },
-  ];
-
-  const users = [
-    {
-      id: 1,
-      name: "Tony Reichert",
-      role: "CEO",
-      team: "Management",
-      status: "active",
-      age: "29",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/1.png",
-      email: "tony.reichert@example.com",
-    },
-    {
-      id: 1,
-      name: "Tony Reichert",
-      role: "CEO",
-      team: "Management",
-      status: "active",
-      age: "29",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/1.png",
-      email: "tony.reichert@example.com",
-    },
-    {
-      id: 1,
-      name: "Tony Reichert",
-      role: "CEO",
-      team: "Management",
-      status: "active",
-      age: "29",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/1.png",
-      email: "tony.reichert@example.com",
-    },
-    {
-      id: 1,
-      name: "Tony Reichert",
-      role: "CEO",
-      team: "Management",
-      status: "active",
-      age: "29",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/1.png",
-      email: "tony.reichert@example.com",
-    },
-    {
-      id: 2,
-      name: "Zoey Lang",
-      role: "Tech Lead",
-      team: "Development",
-      status: "paused",
-      age: "25",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/1.png",
-      email: "zoey.lang@example.com",
-    },
-    {
-      id: 3,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 3,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 5,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 6,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 1,
-      name: "Tony Reichert",
-      role: "CEO",
-      team: "Management",
-      status: "active",
-      age: "29",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/1.png",
-      email: "tony.reichert@example.com",
-    },
-    {
-      id: 2,
-      name: "Zoey Lang",
-      role: "Tech Lead",
-      team: "Development",
-      status: "paused",
-      age: "25",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/1.png",
-      email: "zoey.lang@example.com",
-    },
-    {
-      id: 3,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 3,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 5,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 6,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 1,
-      name: "Tony Reichert",
-      role: "CEO",
-      team: "Management",
-      status: "active",
-      age: "29",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/male/1.png",
-      email: "tony.reichert@example.com",
-    },
-    {
-      id: 2,
-      name: "Zoey Lang",
-      role: "Tech Lead",
-      team: "Development",
-      status: "paused",
-      age: "25",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/1.png",
-      email: "zoey.lang@example.com",
-    },
-    {
-      id: 3,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 3,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 5,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
-    },
-    {
-      id: 6,
-      name: "Jane Fisher",
-      role: "Sr. Dev",
-      team: "Development",
-      status: "active",
-      age: "22",
-      avatar: "https://d2u8k2ocievbld.cloudfront.net/memojis/female/2.png",
-      email: "jane.fisher@example.com",
+      count: pendingAppointments.patientDetails.length,
     },
   ];
 
@@ -264,27 +53,29 @@ export default function PatientTabs() {
             }
           >
             <Listbox
+              aria-label="Patient List"
               classNames={{
-                // base: "max-w-xs",
-                // list: "max-h-[300px] overflow-scroll",
                 list: "max-h-[500px] overflow-y-scroll scrollbar",
               }}
-              items={users}
+              items={pendingAppointments.patientDetails} // Accessing the patientDetails array
               variant="shadow"
             >
-              {(item) => (
-                <ListboxItem key={item.id} textValue={item.name}>
+              {(patient) => (
+                <ListboxItem
+                  key={patient._id}
+                  textValue={`${patient.firstname} ${patient.lastname}`}
+                >
                   <div className="flex gap-2 items-center">
                     <Avatar
-                      alt={item.name}
+                      alt={`${patient.firstname} ${patient.lastname}`}
                       className="flex-shrink-0"
                       size="sm"
-                      src={item.avatar}
+                      src={patient.profile}
                     />
                     <div className="flex flex-col">
-                      <span className="text-small">{item.name}</span>
+                      <span className="text-small">{`${patient.firstname} ${patient.lastname}`}</span>
                       <span className="text-tiny text-default-400">
-                        {item.email}
+                        {patient.email}
                       </span>
                     </div>
                   </div>
