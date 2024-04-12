ed4b4738728bc59f879afee80e6612a36a645160
Anand Suthar <anandsuthar956@gmail.com>
2024-04-12 12:44:48 +0530

    Completed the method with Auth headers

--- lib/patient/getUpcomingAppointments.tsx
+++ lib/patient/getUpcomingAppointments.tsx
@@ -1,8 +1,13 @@
+import { getSessionToken } from "@sessions/sessionUtils";
+
 export default async function getUpcomingAppointments() {
+  const session = await getSessionToken();
+
   const res = await fetch(`http://localhost:3000/api/patient/appointment`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${session}`,
     },
     next: { revalidate: 10 },
   });
