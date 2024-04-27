e12ef6fe11f80edb0c8147fff8922cc6d53e4743
Anand Suthar <anandsuthar956@gmail.com>
2024-04-27 23:18:07 +0530

    Removed search

--- app/(pages)/receptionist/components/Headbar/index.tsx
+++ app/(pages)/receptionist/components/Headbar/index.tsx
@@ -25,35 +25,6 @@ export default async function Headbar() {
         <p className="text-lg font-semibold tracking-wider">
           Patient Fitness Tracker
         </p>
-        <div className="w-2/5">
-          <Input
-            isClearable
-            radius="lg"
-            classNames={{
-              input: [
-                "bg-transparent",
-                "text-black/90 dark:text-white/90",
-                "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-              ],
-              innerWrapper: "bg-transparent",
-              inputWrapper: [
-                "bg-white",
-                "dark:bg-default/60",
-                "backdrop-blur-xl",
-                "backdrop-saturate-200",
-                "hover:bg-default-200/70",
-                "dark:hover:bg-default/70",
-                "group-data-[focused=true]:bg-default-200/50",
-                "dark:group-data-[focused=true]:bg-default/60",
-                "!cursor-text",
-              ],
-            }}
-            placeholder="Type to search..."
-            startContent={
-              <CiSearch className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
-            }
-          />
-        </div>
       </div>
 
       <div className="flex justify-center items-center gap-2">
