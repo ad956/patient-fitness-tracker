a840d1b74663f206faaca97321dca825c58fa731
Anand Suthar <anandsuthar956@gmail.com>
2024-04-27 23:58:12 +0530

    Minor fix for adding receptionist id to approved ones

--- app/api/receptionist/appointments/approve/route.ts
+++ app/api/receptionist/appointments/approve/route.ts
@@ -2,7 +2,7 @@ import dbConfig from "@/lib/db";
 import { decryptSessionToken } from "@sessions/sessionUtils";
 import { ObjectId } from "mongodb";
 
-export async function POST(request: Request) {
+export async function GET(request: Request) {
   try {
     const { patient_id } = await request.json();
 
@@ -12,10 +12,61 @@ export async function POST(request: Request) {
     const db = await dbConfig();
     const bookedAppointmentsCollection = db.collection("bookedAppointments");
 
+    // Fetch the booked appointments for the specific patient and their receptionist
+    const appointments = await bookedAppointmentsCollection
+      .find({
+        patient_id: patientObjectId,
+        // Add the condition to filter by receptionist_id
+        receptionist_id: { $exists: true },
+      })
+      .toArray();
+
+    return new Response(JSON.stringify({ appointments }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error fetching patient appointments:", error);
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+}
+
+export async function POST(request: Request) {
+  const session = request.headers.get("Authorization");
+  if (!session) {
+    return new Response(JSON.stringify({ error: "Unauthorized" }), {
+      status: 401,
+    });
+  }
+
+  try {
+    const { patient_id } = await request.json();
+    const token = session.split("Bearer ")[1];
+    const decryptedUser = await decryptSessionToken(token);
+
+    // Convert the patient_id string to an ObjectId
+    const patientObjectId = new ObjectId(patient_id);
+
+    const db = await dbConfig();
+
+    const receptionistCollection = db.collection("receptionist");
+    const bookedAppointmentsCollection = db.collection("bookedAppointments");
+
+    const receptionist = await receptionistCollection.findOne({
+      email: decryptedUser.user.email,
+    });
+
+    if (!receptionist) {
+      return new Response(JSON.stringify({ error: "Receptionist not found" }), {
+        status: 404,
+      });
+    }
+
     // Update the approved status of the pending appointment for the specific patient to "approved"
     const updateResult = await bookedAppointmentsCollection.updateOne(
       { approved: "pending", patient_id: patientObjectId },
-      { $set: { approved: "approved" } }
+      { $set: { approved: "approved", receptionist_id: receptionist._id } }
     );
 
     // Check if any document was updated
