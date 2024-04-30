47335e919b91ecc5213c793d9c4c32816c7356c6
Anand Suthar <anandsuthar956@gmail.com>
2024-04-30 13:26:07 +0530

    Component completed with responsiveness

--- app/(pages)/patient/components/MedicineDetails/index.tsx
+++ app/(pages)/patient/components/MedicineDetails/index.tsx
@@ -1,3 +1,4 @@
+import { Image } from "@nextui-org/react";
 import React from "react";
 
 type MedicineDetailsProp = {
@@ -14,28 +15,22 @@ type MedicineDetailsProp = {
 
 export default function MedicineDetails({ details }: MedicineDetailsProp) {
   return (
-    <div className="w-full px-2">
+    <div
+      className={`flex flex-row flex-wrap px-2 ${
+        details.length > 4 ? "overflow-y-scroll scrollbar" : ""
+      }`}
+    >
       {details.map((medicine, index) => (
-        <div key={index} className="flex flex-row gap-2 p-2">
-          <div className="bg-default textwhite p-2 rounded-xl h-10">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-medicine-syrup"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M8 21h8a1 1 0 0 0 1 -1v-10a3 3 0 0 0 -3 -3h-4a3 3 0 0 0 -3 3v10a1 1 0 0 0 1 1z" />
-              <path d="M10 14h4" />
-              <path d="M12 12v4" />
-              <path d="M10 7v-3a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v3" />
-            </svg>
+        <div
+          key={index}
+          className="flex flex-row gap-2 p-2"
+          style={{ width: "50%" }}
+        >
+          <div className="p-2 rounded-xl">
+            <Image
+              src={`/icons/${index % 3 === 0 ? "tablets.png" : "tablets2.png"}`}
+              className="h-10"
+            />
           </div>
 
           <div className="flex flex-col justify-center">
