1ec3d38d0d721237adfb9da82406f1fc33f2b8c7
Anand Suthar <anandsuthar956@gmail.com>
2024-05-01 23:51:03 +0530

    Implemented error boundary

--- app/(pages)/patient/error.tsx
+++ app/(pages)/patient/error.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Image, Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+
 export default function Error({
   error,
   reset,
@@ -7,11 +10,26 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  const router = useRouter();
+
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <p>here what gone wrong : {error.message}</p>
-      <button onClick={() => reset()}>Try again</button>
+    <div className="flex justify-center gap-5 items-center">
+      <div className="flex flex-col gap-5">
+        <h2 className="text-[#606060] font-bold tracking-wider text-5xl">
+          Oops! Sorry <br />
+        </h2>
+        <p className="font-medium text-2xl text-black/55">{error.message}</p>
+        <Button
+          onClick={() => router.replace("/")}
+          className="bg-[#00b09c] text-[#e5e5dd] text-md font-semibold my-5 max-w-32"
+        >
+          Go Back
+        </Button>
+      </div>
+
+      <div className="h-full">
+        <Image alt="error-image" src="/images/error.jpg" className="h-full" />
+      </div>
     </div>
   );
 }
