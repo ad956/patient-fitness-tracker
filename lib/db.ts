5b96be2e14acc9edde0d73f4c7e0247da995c6ad
Anand Suthar <anandsuthar956@gmail.com>
2024-05-03 21:35:17 +0530

    minor

--- lib/db.ts
+++ lib/db.ts
@@ -4,7 +4,7 @@ export default async function dbConfig() {
   const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/";
   const client = await MongoClient.connect(uri);
   if (!client) {
-    throw new Error("Function not implemented.");
+    throw new Error("Error connecting mongodb.");
   }
   const db = client.db("pft");
 
