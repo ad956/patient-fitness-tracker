c8414bf54a8f73ca4278efd37b3647ec51977d7d
Anand Suthar <anandsuthar956@gmail.com>
2024-05-03 19:35:22 +0530

    Minor

--- app/layouts/LandingPage/components/brandlogo/index.tsx
+++ app/layouts/LandingPage/components/brandlogo/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const BrandLogo = () => (
   <div className="flex items-center gap-2">
-    <Image src="patient.svg" height="40" width="40" alt="brand-logo" />
+    <Image src="/patient.svg" height="40" width="40" alt="brand-logo" />
     <h2 className="text-xl font-extrabold">Patient Fitness Tracker</h2>
   </div>
 );
