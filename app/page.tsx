cbb35bc88e95cf4c24f65d770d45e7062fce1674
Anand Suthar <anandsuthar956@gmail.com>
2024-05-03 19:35:00 +0530

    Redirection logic removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import { redirect } from "next/navigation";
 import LandingPage from "./layouts/LandingPage";
-import { getSession } from "@/lib/sessions/sessionUtils";
 
 export default async function Home() {
-  const isAuthenticated = await getSession();
-  const userRole = isAuthenticated?.user.role;
-
-  // if (!userRole) throw new Error("User not authentuicated");
-
-  return isAuthenticated ? redirect(`/${userRole}`) : <LandingPage />;
+  return <LandingPage />;
 }
