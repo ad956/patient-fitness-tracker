4d35667392ced0256196273995664ec69f407e32
Anand Suthar <anandsuthar956@gmail.com>
2024-05-05 22:54:22 +0530

    extracting components from index

---
+++ app/layouts/LandingPage/components/index.tsx
@@ -0,0 +1,25 @@
+import About from "./about";
+import NavBar from "./navbar";
+import ContactUS from "./contact";
+import Features from "./features";
+import Footer from "./footer";
+import Intro from "./intro";
+import Lifeline from "./lifeline";
+import ServicesOffered from "./services";
+import Testimonial from "./testimonial";
+import TrustedBy from "./trustedby";
+import WorkingProcess from "./working";
+
+export {
+  About,
+  NavBar,
+  ContactUS,
+  Features,
+  Footer,
+  Intro,
+  Lifeline,
+  ServicesOffered,
+  Testimonial,
+  TrustedBy,
+  WorkingProcess,
+};
--- app/layouts/LandingPage/index.tsx
+++ app/layouts/LandingPage/index.tsx
@@ -1,41 +1,43 @@
 import { Card, CardFooter, Image, Button, Divider } from "@nextui-org/react";
-import About from "./components/about";
-import ContactUS from "./components/contact";
-import Features from "./components/features";
-import Footer from "./components/footer";
-import Intro from "./components/intro";
-import Lifeline from "./components/lifeline";
-import NavBar from "./components/navbar";
-import ServicesOffered from "./components/services";
-import Testimonial from "./components/testimonial";
-import TrustedBy from "./components/trustedby";
-import WorkingProcess from "./components/working";
+import {
+  About,
+  ContactUS,
+  Features,
+  Footer,
+  Intro,
+  Lifeline,
+  NavBar,
+  ServicesOffered,
+  Testimonial,
+  TrustedBy,
+  WorkingProcess,
+} from "./components";
 
 export default function LandingPage() {
-	return (
-		<main className="scroll-smooth h-[100vh]">
-			<NavBar />
-			<Intro />
-			<Lifeline />
+  return (
+    <main className="scroll-smooth h-[100vh]">
+      <NavBar />
+      <Intro />
+      <Lifeline />
 
-			<section className="my-10 flex justify-center">
-				<Divider className="w-4/5 bg-gray-400" />
-			</section>
+      <section className="my-10 flex justify-center">
+        <Divider className="w-4/5 bg-gray-400" />
+      </section>
 
-			<section className="flex justify-center items-center">
-				<TrustedBy />
-			</section>
+      <section className="flex justify-center items-center">
+        <TrustedBy />
+      </section>
 
-			<Features />
-			<WorkingProcess />
+      <Features />
+      <WorkingProcess />
 
-			<ServicesOffered />
+      <ServicesOffered />
 
-			<Testimonial />
+      <Testimonial />
 
-			<About />
-			<ContactUS />
-			<Footer />
-		</main>
-	);
+      <About />
+      <ContactUS />
+      <Footer />
+    </main>
+  );
 }
