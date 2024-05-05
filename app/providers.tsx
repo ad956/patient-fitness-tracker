40376a00ec35410bf2f75e0d956946231a92f186
Anand Suthar <anandsuthar956@gmail.com>
2024-05-05 22:53:17 +0530

    Added error boundary

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import ErrorBoundary from "@components/error-boundary"; // Import the ErrorBoundary component
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <NextUIProvider>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider>
+      <ErrorBoundary>{children}</ErrorBoundary>
+    </NextUIProvider>
+  );
 }
