3e6a531ac9b1035b4082e8f8658486da94acfc8b
Anand Suthar <anandsuthar956@gmail.com>
2024-05-06 23:17:35 +0530

    Added baseUrl to config

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,12 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-console.log(BASE_URL);
-
 test("Displays the landing page correctly", async ({ page }) => {
   // Navigate to the home page
-  await page.goto(`${BASE_URL}/`);
+  await page.goto("/");
 
   // Expect the main heading to be visible
   await expect(
