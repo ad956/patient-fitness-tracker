61e0f4b0bb7d21dad4f9039a3ac09c0bcf4958b1
Anand Suthar <anandsuthar956@gmail.com>
2024-05-06 21:20:16 +0530

    minor fix

--- app/utils/getBaseUrl.ts
+++ app/utils/getBaseUrl.ts
@@ -1,12 +1,12 @@
 let cachedBaseUrl: string;
 
 export default function getBaseUrl() {
-  if (cachedBaseUrl) {
+  if (cachedBaseUrl !== undefined) {
     return cachedBaseUrl;
   }
 
   if (process.env.NODE_ENV === "production") {
-    cachedBaseUrl = process.env.BASE_URL || "http://localhost:3000";
+    cachedBaseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
   } else {
     cachedBaseUrl = "http://localhost:3000";
   }
