b626bd9ac4b19cc76b908be3a72b8b94e748f8ed
Anand Suthar <anandsuthar956@gmail.com>
2024-06-08 23:16:22 +0530

    Accessing patient data in server component and passing to its child

--- app/(pages)/patient/settings/page.tsx
+++ app/(pages)/patient/settings/page.tsx
@@ -1,9 +1,13 @@
+import { Patient } from "@/types";
+import { getPatientData } from "@lib/patient";
 import { ProfileSettings } from "../components";
 
-export default function Settings() {
+export default async function Settings() {
+  const patient: Patient = await getPatientData();
+
   return (
     <section className="h-full w-full flex flex-col">
-      <ProfileSettings />
+      <ProfileSettings patient={patient} />
     </section>
   );
 }
