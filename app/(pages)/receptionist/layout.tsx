9c22fac398ac1900ab274044e9583ba78de43100
Anand Suthar <anandsuthar956@gmail.com>
2024-06-18 21:52:07 +0530

    refactor: passing the necessary user and role props to the Headbar component

--- app/(pages)/receptionist/layout.tsx
+++ app/(pages)/receptionist/layout.tsx
@@ -1,21 +1,28 @@
-import { Headbar, Sidebar } from "./components";
+import { Receptionist } from "@/types";
+import { Sidebar } from "./components";
+import { Headbar } from "@components/index";
+
 import type { Metadata } from "next";
+import { getReceptionistData } from "@lib/receptionist";
 
 export const metadata: Metadata = {
   title: "Patient Fitness Tracker",
   description: "The page is for receptionist related applications.",
 };
 
-export default function PatientLayout({
+export default async function PatientLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const receptionist: Receptionist = await getReceptionistData();
+
   return (
     <main className="h-[100vh] flex">
       <Sidebar />
       <section className="flex flex-col w-full">
-        <Headbar />
+        <Headbar user={receptionist} role="receptionist" />
+
         {children}
       </section>
     </main>
