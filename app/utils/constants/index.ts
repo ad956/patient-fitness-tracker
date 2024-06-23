19aeab21448d519a66ba87eebc7c627b7e9be1fa
Anand Suthar <anandsuthar956@gmail.com>
2024-06-23 15:07:02 +0530

    minor changes Due to moving icons & images

--- app/utils/constants/index.ts
+++ app/utils/constants/index.ts
@@ -1,16 +1,16 @@
 const carouselData = [
   {
-    image: "/reception.png",
+    image: "/images/reception.png",
     title: "Efficient Patient Management",
     desc: " Receptionist dashboard for streamlined appointment and record management",
   },
   {
-    image: "/doctor.png",
+    image: "/images/doctor.png",
     title: "Personalized Care Delivery",
     desc: "Doctor interface for tailored treatment plans and patient care.",
   },
   {
-    image: "/admin.png",
+    image: "/images/admin.png",
     title: "Comprehensive Admin Tools",
     desc: "Admin dashboard for seamless hospital operations and performance monitoring.",
   },
