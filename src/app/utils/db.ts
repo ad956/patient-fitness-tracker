f1531190f85daa234eb5e9478ce037aa60baebd9
Anand Suthar <anandsuthar956@gmail.com>
2024-07-02 01:41:25 +0530

    Using mongoose for db connection &  improved dbConfig

---
+++ src/app/utils/db.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const connection: { isConnected?: number } = {};
+
+let uri: string;
+if (process.env.NODE_ENV === "production") {
+  uri = process.env.MONGODB_URI || "mongodb://localhost:27017/";
+} else {
+  uri = "mongodb://localhost:27017/";
+}
+
+export default async function dbConfig() {
+  if (connection.isConnected) {
+    return;
+  }
+
+  const db = await mongoose.connect(uri, {
+    dbName: "pft",
+  });
+
+  connection.isConnected = db.connections[0].readyState;
+}
