f7a39383e1d0bc4a23564b2e058a631b4ba022fc
Anand Suthar <anandsuthar956@gmail.com>
2024-07-09 17:18:21 +0530

    refactor : landing page components structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,43 @@
-import LandingPage from "./layouts/LandingPage";
+import { Divider } from "@nextui-org/react";
+import {
+  About,
+  ContactUS,
+  Features,
+  Footer,
+  Intro,
+  Lifeline,
+  NavBar,
+  ServicesOffered,
+  Testimonial,
+  TrustedBy,
+  WorkingProcess,
+} from "./components/landing-page";
 
-export default async function Home() {
-  return <LandingPage />;
+export default function LandingPage() {
+  return (
+    <main className="scroll-smooth h-screen">
+      <NavBar />
+      <Intro />
+      <Lifeline />
+
+      <section className="my-10 flex justify-center">
+        <Divider className="w-4/5 bg-gray-400" />
+      </section>
+
+      <section className="flex justify-center items-center">
+        <TrustedBy />
+      </section>
+
+      <Features />
+      <WorkingProcess />
+
+      <ServicesOffered />
+
+      <Testimonial />
+
+      <About />
+      <ContactUS />
+      <Footer />
+    </main>
+  );
 }
