4c977b991623fd1f9899f9c5b06f7541dbedf81f
Anand Suthar <anandsuthar956@gmail.com>
2024-07-11 00:12:38 +0530

    fix : unwanted _id issue in address object and - overwriting whole address issue

--- app/api/patient/update-profile/address/route.ts
+++ app/api/patient/update-profile/address/route.ts
@@ -1,4 +1,6 @@
+import dbConfig from "@utils/db";
 import Patient from "@models/patient";
+import { decrypt } from "@sessions/sessionUtils";
 
 type AddressBody = {
   address_line_1?: string;
@@ -10,30 +12,52 @@ type AddressBody = {
 };
 
 export async function PUT(req: Request) {
-  const email = "anandsuthar956@gmail.com";
+  await dbConfig();
 
-  const addressData: AddressBody = await req.json();
-
-  // Remove undefined fields
-  Object.keys(addressData).forEach((key) => {
-    if (addressData[key as keyof AddressBody] === undefined) {
-      delete addressData[key as keyof AddressBody];
-    }
-  });
+  const session = req.headers.get("Authorization");
+  if (!session) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
   try {
-    const updatedPatient = await Patient.findOneAndUpdate(
-      { email },
-      { $set: { address: addressData } },
-      { new: true }
-    );
+    const token = session.split("Bearer ")[1];
+    const decryptedUser = await decrypt(token);
+    const email = decryptedUser.user.email;
 
-    if (!updatedPatient) {
+    const addressData: AddressBody = await req.json();
+
+    // removing undefined fields
+    Object.keys(addressData).forEach((key) => {
+      if (addressData[key as keyof AddressBody] === undefined) {
+        delete addressData[key as keyof AddressBody];
+      }
+    });
+
+    const patient = await Patient.findOne({ email });
+
+    if (!patient) {
       return Response.json({ error: "Patient not found" }, { status: 404 });
     }
 
+    const updatedAddress = {
+      address_line_1: patient.address.address_line_1,
+      address_line_2: patient.address.address_line_2,
+      city: patient.address.city,
+      state: patient.address.state,
+      zip_code: patient.address.zip_code,
+      country: patient.address.country,
+      ...addressData,
+    };
+
+    patient.address = updatedAddress;
+
+    console.table(patient.address);
+
+    await patient.save();
+
     return Response.json({ msg: "ok" }, { status: 200 });
   } catch (error) {
+    console.error("Error updating address:", error);
     return Response.json({ error: "Error updating address" }, { status: 500 });
   }
 }
