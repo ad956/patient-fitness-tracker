7c3936818260fe2e082e08e457db0ae3188fdecf
Anand Suthar <anandsuthar956@gmail.com>
2024-07-11 23:25:20 +0530

    refactor : updated profile settings for receptionist

--- app/(pages)/receptionist/settings/page.tsx
+++ app/(pages)/receptionist/settings/page.tsx
@@ -1,9 +1,13 @@
-import { ProfileSettings } from "../components";
+import ProfileSettings from "@components/profile-settings";
+import { getReceptionistData } from "@lib/receptionist";
+import { Receptionist } from "@types";
 
-export default function Settings() {
+export default async function Settings() {
+  const receptionist: Receptionist = await getReceptionistData();
+
   return (
     <section className="h-full w-full flex flex-col">
-      <ProfileSettings />
+      <ProfileSettings user={receptionist} />
     </section>
   );
 }
