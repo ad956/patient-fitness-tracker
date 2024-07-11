2a4b729099260cfdf45fe065feb6412c59bf83dc
Anand Suthar <anandsuthar956@gmail.com>
2024-07-11 23:25:48 +0530

    refactor : minor changes

--- app/(pages)/receptionist/components/index.tsx
+++ app/(pages)/receptionist/components/index.tsx
@@ -1,12 +1,5 @@
 import { MonthlyVisitors, WeeklyProgress } from "./Graphs";
 import PatientTabs from "./PatientTabs";
-import ProfileSettings from "./ProfileSettings";
 import Sidebar from "./Sidebar";
 
-export {
-  MonthlyVisitors,
-  WeeklyProgress,
-  PatientTabs,
-  ProfileSettings,
-  Sidebar,
-};
+export { MonthlyVisitors, WeeklyProgress, PatientTabs, Sidebar };
