b465859464f752cf1313791ffc7d62626651c454
Anand Suthar <anandsuthar956@gmail.com>
2024-07-13 22:33:21 +0530

    renamed update profile picture method

--- app/lib/update-profile/index.ts
+++ app/lib/update-profile/index.ts
@@ -3,6 +3,6 @@
 import updateAddress from "./update_address";
 import updatePersonal from "./update_personal_info";
 import resetPassword from "./reset_password";
-import updateProfilePicture from "./updateProfilePicture";
+import updateProfilePicture from "./update_profile_picture";
 
 export { updateAddress, updatePersonal, resetPassword, updateProfilePicture };
