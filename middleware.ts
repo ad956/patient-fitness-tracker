eb5d365f94c4d50da3dfd1db259d43ebc7da1636
Anand Suthar <anandsuthar956@gmail.com>
2024-07-20 22:46:50 +0530

    fix : handleApiRoute Not Found

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { updateSession } from "@sessions/sessionUtils";
 import {
-  handleApiRoute,
   handleExpiredSession,
   handlePrivateRoute,
   handlePublicRoute,
