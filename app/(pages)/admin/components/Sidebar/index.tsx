642c4f2523f4cfe05a355fb8ed022899cc862bbc
Anand Suthar <anandsuthar956@gmail.com>
2024-07-27 18:45:31 +0530

    refactor : Add logic to persist selected sidebar icon

--- app/(pages)/admin/components/Sidebar/index.tsx
+++ app/(pages)/admin/components/Sidebar/index.tsx
@@ -1,47 +1,82 @@
 "use client";
 
-import { Image } from "@nextui-org/react";
+import React, { useState } from "react";
 import Link from "next/link";
-import React from "react";
+import { Image } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+import getBaseUrl from "@utils/getBaseUrl";
 import {
-  FiCalendar,
+  FiHome,
+  FiUserPlus,
   FiUser,
   FiBarChart2,
   FiSettings,
-  FiGrid,
+  FiHeart,
 } from "react-icons/fi";
+import { TbCurrencyRupee } from "react-icons/tb";
 
-const Sidebar = () => (
-  <aside className="h-screen flex-col border-r bg-white w-16 shadow-sm">
-    <nav className="flex flex-col items-center gap-6 px-2 py-5">
-      <Image
-        src="/icons/patient.svg"
-        height={35}
-        width={35}
-        alt="Patient Fitness Tracker"
-      />
-      <SidebarIcon icon={<FiGrid />} isActive />
-      <SidebarIcon icon={<FiUser />} />
-      <SidebarIcon icon={<FiCalendar />} />
-      <SidebarIcon icon={<FiBarChart2 />} />
-      <SidebarIcon icon={<FiSettings />} />
-    </nav>
-    <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
-      <SidebarIcon icon={<FiUser />} href="/admin/settings" />
-    </nav>
-  </aside>
-);
+// Sidebar configuration
+const SidebarConfig = [
+  { title: "Dashboard", uri: "", icon: <FiHome /> },
+  { title: "Add Admin", uri: "add-admin", icon: <FiUserPlus /> },
+  { title: "Billing", uri: "billing", icon: <TbCurrencyRupee /> },
+  { title: "Hospitals", uri: "hospitals", icon: <FiHeart /> },
+  { title: "Users", uri: "users", icon: <FiUser /> },
+  { title: "Reports", uri: "reports", icon: <FiBarChart2 /> },
+  { title: "Settings", uri: "settings", icon: <FiSettings /> },
+];
 
-const SidebarIcon = ({ icon, isActive = false, href = "#" }: any) => (
+const Sidebar = () => {
+  const pathname = usePathname();
+  const [selected, setSelected] = useState<number>(
+    getInitialSelectedIndex(pathname)
+  );
+
+  function getInitialSelectedIndex(pathname: string): number {
+    const index = SidebarConfig.findIndex(
+      (item) => `/admin/${item.uri}` === pathname
+    );
+    return index !== -1 ? index : 0;
+  }
+
+  function handleButtonClick(index: number): void {
+    setSelected(index);
+  }
+
+  return (
+    <aside className="h-screen flex-col border-r bg-white w-16 shadow-sm">
+      <nav className="flex flex-col items-center gap-6 px-2 py-5">
+        <Image
+          src="/icons/patient.svg"
+          height={35}
+          width={35}
+          alt="Patient Fitness Tracker"
+        />
+        {SidebarConfig.map((item, index) => (
+          <SidebarIcon
+            key={item.title}
+            icon={item.icon}
+            href={`/admin/${item.uri}`}
+            isActive={selected === index}
+            onClick={() => handleButtonClick(index)}
+          />
+        ))}
+      </nav>
+    </aside>
+  );
+};
+
+const SidebarIcon = ({ icon, isActive = false, href = "#", onClick }: any) => (
   <Link
     href={href}
+    onClick={onClick}
     className={`flex h-10 w-10 items-center justify-center rounded-lg transition-colors duration-200 ${
       isActive
         ? "bg-warning-100 text-warning-600"
         : "text-gray-600 hover:bg-gray-100 hover:text-gray-900"
     }`}
   >
-    {React.cloneElement(icon, { className: "h-5 w-5" })}
+    {React.cloneElement(icon, { className: "h-5 w-5 stroke-[1.5]" })}
   </Link>
 );
 
