fe6af7e5b7fe7d26727e193f7e84343f2318c213
Anand Suthar <anandsuthar956@gmail.com>
2024-08-07 20:04:33 +0530

    refactor: import paginationControls component in index.ts

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -4,12 +4,12 @@ import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import Headbar from "./Headbar";
 import OtpSection from "./OtpSection";
+import PaginationControls from "./PaginationControls";
 import ProfileSettings from "./ProfileSettings";
 import SearchInput from "./SearchInput";
 import Sidebar from "./Sidebar";
 import SpinnerLoader from "./SpinnerLoader";
 import StatusFilter from "./StatusFilter";
-import TablePagination from "./PaginationControls";
 import TransactionsTable from "./TransactionsTable";
 
 export {
@@ -19,11 +19,11 @@ export {
   ErrorBoundary,
   Headbar,
   OtpSection,
+  PaginationControls,
   ProfileSettings,
   SearchInput,
   Sidebar,
   SpinnerLoader,
   StatusFilter,
-  TablePagination,
   TransactionsTable,
 };
