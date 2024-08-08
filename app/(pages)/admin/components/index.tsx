5b591f0089fb88c08ba36bd878b6a81aac9b9e9e
Anand Suthar <anandsuthar956@gmail.com>
2024-08-08 22:30:02 +0530

    refactor : add Transactions component

--- app/(pages)/admin/components/index.tsx
+++ app/(pages)/admin/components/index.tsx
@@ -3,5 +3,13 @@ import LoginForm from "./LoginForm";
 import Header from "./Header";
 import PinVerification from "./PinVerification";
 import Sidebar from "./Sidebar";
+import Transactions from "./Transactions";
 
-export { AccessDenied, LoginForm, Header, PinVerification, Sidebar };
+export {
+  AccessDenied,
+  LoginForm,
+  Header,
+  PinVerification,
+  Sidebar,
+  Transactions,
+};
