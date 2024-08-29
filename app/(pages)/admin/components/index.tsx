0977c8f0cd3d30827f7b6d1dc6a8cd8859c6a33e
Anand Suthar <anandsuthar956@gmail.com>
2024-08-29 22:39:57 +0530

    feat : Add RecentActivity and StatsCard Component

--- app/(pages)/admin/components/index.tsx
+++ app/(pages)/admin/components/index.tsx
@@ -2,7 +2,9 @@ import AccessDenied from "./AccessDenied";
 import LoginForm from "./LoginForm";
 import Header from "./Header";
 import PinVerification from "./PinVerification";
+import RecentActivity from "./RecentActivity";
 import Sidebar from "./Sidebar";
+import StatisticsCards from "./StatisticsCards";
 import Transactions from "./Transactions";
 
 export {
@@ -10,6 +12,8 @@ export {
   LoginForm,
   Header,
   PinVerification,
+  RecentActivity,
   Sidebar,
+  StatisticsCards,
   Transactions,
 };
