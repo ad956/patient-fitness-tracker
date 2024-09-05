8ad098662ccb038ae6b09b8f0963a732b44adece
Anand Suthar <anandsuthar956@gmail.com>
2024-09-05 22:26:49 +0530

    refactor: update section overflow style

--- app/(pages)/receptionist/settings/page.tsx
+++ app/(pages)/receptionist/settings/page.tsx
@@ -6,7 +6,7 @@ export default async function Settings() {
   const receptionist: Receptionist = await getReceptionistData();
 
   return (
-    <section className="h-full w-full flex flex-col">
+    <section className="h-full w-full flex flex-col overflow-y-auto">
       <ProfileSettings user={receptionist} />
     </section>
   );
