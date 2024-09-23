b7d2a223c8c79d5ba993241624fa70d7ecbfea82
Anand Suthar <anandsuthar956@gmail.com>
2024-09-23 20:12:16 +0530

    chore(middlewares): remove handleApiRoute middleware 🗑️

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,11 +1,5 @@
-import handleApiRoute from "./handleApiRoute";
 import handleExpiredSession from "./handleExpiredSession";
 import handlePrivateRoute from "./handlePrivateRoute";
 import handlePublicRoute from "./handlePublicRoute";
 
-export {
-  handleApiRoute,
-  handleExpiredSession,
-  handlePrivateRoute,
-  handlePublicRoute,
-};
+export { handleExpiredSession, handlePrivateRoute, handlePublicRoute };
