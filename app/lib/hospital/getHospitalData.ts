32869baa6eed442faa6a7f5696c1cc0929b43df7
Anand Suthar <anandsuthar956@gmail.com>
2024-09-25 23:42:20 +0530

    feat(hospital): create hospital data fetching API with enhanced type and session handling 🏥
    - Added API method for fetching hospital data using fetchHandler
    - Ensured type safety for hospital data structures
    - Passed session token for secure API requests and used cache control options

--- app/lib/hospital/getHospitalData.ts
+++ app/lib/hospital/getHospitalData.ts
@@ -1,14 +1,27 @@
-import fetchHandler from "@utils/fetchHandler";
+"use server";
 
-export default async function getHospitalData() {
+import fetchHandler from "@utils/fetchHandler";
+import { getSessionToken } from "../sessions/sessionUtils";
+import { Hospital } from "@pft-types/index";
+
+export default async function getHospitalData(): Promise<Hospital> {
   const endpoint = "/api/hospital";
+  const session = getSessionToken();
 
   try {
-    const hospitalData = await fetchHandler(endpoint, {
-      cache: "no-cache",
-    });
+    const response = await fetchHandler<Hospital>(
+      endpoint,
+      {
+        cache: "no-cache",
+      },
+      session!
+    );
 
-    return hospitalData;
+    if (response.error) {
+      throw new Error(response.error.message);
+    }
+
+    return response.data!;
   } catch (error) {
     console.error("An error occurred while fetching hospital data:", error);
     throw error;
