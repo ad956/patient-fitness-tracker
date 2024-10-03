37b2eeee26eb1b22f9587b3b43067559b56b6c4f
Anand Suthar <anandsuthar956@gmail.com>
2024-10-03 23:23:55 +0530

    chore: remove test api route

--- app/api/test/route.ts
+++
@@ -1,6 +0,0 @@
-import { setSession } from "@/lib/sessions/sessionUtils";
-
-export async function GET() {
-  await setSession("66b72575fc219b2351f17180", "admin");
-  return Response.json("OK");
-}
