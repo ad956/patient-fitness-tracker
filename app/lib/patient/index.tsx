b0653b585e7a103fbc47ae556c66b0e1763644dd
Anand Suthar <anandsuthar956@gmail.com>
2024-10-23 21:21:29 +0530

    feat: Add method to fetch pending bills of patient

--- app/lib/patient/index.tsx
+++ app/lib/patient/index.tsx
@@ -1,6 +1,7 @@
 import bookAppointment from "./bookAppointment";
 import getPatientData from "./getPatientData";
 import getPatientMedicalHistory from "./getPatientMedicalHistory";
+import getPendingBills from "./getPendingBills";
 import getPaymentsHistory from "./getPaymentsHistory";
 import getUpcomingAppointments from "./getUpcomingAppointments";
 import pendingAppointmentsRequest from "./pendingAppointmentsReq";
@@ -10,6 +11,7 @@ export {
   bookAppointment,
   getPatientData,
   getPatientMedicalHistory,
+  getPendingBills,
   getPaymentsHistory,
   getUpcomingAppointments,
   pendingAppointmentsRequest,
