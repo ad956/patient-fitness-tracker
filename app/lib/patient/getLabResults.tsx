2daba8c0b3e59798bc0bccfa99ab71b5156cb3e5
Anand Suthar <anandsuthar956@gmail.com>
2024-10-28 23:02:46 +0530

    🔄 refactor: Remove mock lab results data and implement actual API request - Replaced mock lab results data with a real API request in getLabResults - Utilized etchHandler with session token for authenticated data retrieval - Enhanced error handling for improved debug information

--- app/lib/patient/getLabResults.tsx
+++ app/lib/patient/getLabResults.tsx
@@ -5,24 +5,21 @@ import { getSessionToken } from "../sessions/sessionUtils";
 import { LabResult } from "@pft-types/patient";
 
 export default async function getLabResults(): Promise<LabResult[]> {
-  const endpoint = "/api/patient/paymenthistory?status=pending";
+  const endpoint = "/api/patient/dashboard/labresults";
   const session = getSessionToken();
 
   try {
-    // const response = await fetchHandler<LabResult[]>(
-    //   endpoint,
-    //   {
-    //     cache: "no-cache",
-    //   },
-    //   session!
-    // );
-    // if (response.error) {
-    //   throw new Error(response.error.message);
-    // }
-    // return response.data!;
-
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    return labResults;
+    const response = await fetchHandler<LabResult[]>(
+      endpoint,
+      {
+        cache: "no-cache",
+      },
+      session!
+    );
+    if (response.error) {
+      throw new Error(response.error.message);
+    }
+    return response.data!;
   } catch (error) {
     console.error(
       "An error occurred while fetching lab results for patient : ",
@@ -31,27 +28,3 @@ export default async function getLabResults(): Promise<LabResult[]> {
     throw error;
   }
 }
-
-const labResults: LabResult[] = [
-  {
-    id: "LR-001",
-    test: "Complete Blood Count",
-    date: "2024-10-14",
-    status: "Completed",
-    result: "Normal",
-  },
-  {
-    id: "LR-002",
-    test: "Lipid Profile",
-    date: "2024-10-15",
-    status: "Pending",
-    result: "Awaiting",
-  },
-  {
-    id: "LR-003",
-    test: "Thyroid Function",
-    date: "2024-10-16",
-    status: "Processing",
-    result: "In Progress",
-  },
-];
