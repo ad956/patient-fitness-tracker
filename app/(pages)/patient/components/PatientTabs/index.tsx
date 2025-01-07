100f873cfa11c0e407f0e7e1a9e04487ab134467
Anand Suthar <anandsuthar956@gmail.com>
2025-01-07 22:50:44 +0530

    feat: 💬 integrated ChatScreen component with patient and doctor data

--- app/(pages)/patient/components/PatientTabs/index.tsx
+++ app/(pages)/patient/components/PatientTabs/index.tsx
@@ -4,13 +4,13 @@ import React, { useState } from "react";
 import { Tabs, Tab, Card } from "@nextui-org/react";
 import { PatientTabsKey } from "@pft-types/patient";
 import { Patient } from "@pft-types/index";
-import DoctorChat from "../DoctorChat";
 import PendingBills from "../PendingBills";
 import LabResults from "../LabResults";
 import { Toaster } from "react-hot-toast";
 import { IoFlaskOutline } from "react-icons/io5";
 import { RiStethoscopeLine } from "react-icons/ri";
 import { MdReceiptLong } from "react-icons/md";
+import ChatScreen from "@/components/ChatScreen";
 
 interface PatientTabsProps {
   patient: Patient;
@@ -70,7 +70,58 @@ export default function PatientTabs({ patient }: PatientTabsProps) {
             </div>
           }
         >
-          <DoctorChat />
+          <ChatScreen
+            currentUser={{
+              id: patient._id,
+              firstname: patient.firstname,
+              lastname: patient.firstname,
+              specialty: "",
+              profile: patient.profile,
+              role: "patient",
+            }}
+            chatList={[
+              {
+                id: "66b5e0aeea614d6d3301c858",
+                firstname: "Dr. Alice Johnson",
+                lastname: "Dr. Alice Johnson",
+                specialty: "Cardiology",
+                profile: "https://randomuser.me/api/portraits/women/1.jpg",
+                role: "doctor",
+              },
+              {
+                id: "102",
+                firstname: "Dr. Mark Smith",
+                lastname: "Dr. Mark Smith",
+                specialty: "Neurology",
+                profile: "https://randomuser.me/api/portraits/men/2.jpg",
+                role: "doctor",
+              },
+              {
+                id: "103",
+                firstname: "Dr. Emily Davis",
+                lastname: "Dr. Emily Davis",
+                specialty: "Pediatrics",
+                profile: "https://randomuser.me/api/portraits/women/3.jpg",
+                role: "doctor",
+              },
+              {
+                id: "104",
+                firstname: "Dr. James Brown",
+                lastname: "Dr. James Brown",
+                specialty: "Orthopedics",
+                profile: "https://randomuser.me/api/portraits/men/4.jpg",
+                role: "doctor",
+              },
+              {
+                id: "105",
+                firstname: "Dr. Olivia Taylor",
+                lastname: "Dr. Olivia Taylor",
+                specialty: "Dermatology",
+                profile: "https://randomuser.me/api/portraits/women/5.jpg",
+                role: "doctor",
+              },
+            ]}
+          />
         </Tab>
 
         <Tab
