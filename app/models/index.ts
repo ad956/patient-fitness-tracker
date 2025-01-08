874a952d840e3d9d2a58f6c0200fa4bf42e0d379
Anand Suthar <anandsuthar956@gmail.com>
2025-01-08 12:46:03 +0530

    chore: rename files to kebab case

--- app/models/index.ts
+++ app/models/index.ts
@@ -1,16 +1,16 @@
 import Admin from "./admin";
-import BookedAppointment from "./bookedAppointment";
-import CommonDiseases from "./commonDisease";
-import DemoUser from "./demouser";
+import BookedAppointment from "./booked-appointment";
+import CommonDiseases from "./common-disease";
+import DemoUser from "./demo-user";
 import Doctor from "./doctor";
 import Hospital from "./hospital";
-import MedicalHistory from "./medicalHistory";
+import MedicalHistory from "./medical-history";
 import Message from "./message";
-import CityStateHospital from "./citystate_hospitals";
+import CityStateHospital from "./city-state-hospitals";
 import Patient from "./patient";
 import Receptionist from "./receptionist";
 import Transaction from "./transaction";
-import UserLog from "./userLog";
+import UserLog from "./user-log";
 
 export {
   Admin,
