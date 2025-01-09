10518981335f45b9df060d41b7f838ab7314ca4e
Anand Suthar <anandsuthar956@gmail.com>
2025-01-09 22:19:49 +0530

    feat: add <ChatScreen/> component

--- app/(pages)/patient/components/PatientTabs/index.tsx
+++ app/(pages)/patient/components/PatientTabs/index.tsx
@@ -72,55 +72,13 @@ export default function PatientTabs({ patient }: PatientTabsProps) {
         >
           <ChatScreen
             currentUser={{
-              id: patient._id,
+              _id: patient._id,
               firstname: patient.firstname,
-              lastname: patient.firstname,
+              lastname: patient.lastname,
               specialty: "",
               profile: patient.profile,
-              role: "patient",
+              role: "Patient",
             }}
-            chatList={[
-              {
-                id: "66b5e0aeea614d6d3301c858",
-                firstname: "Dr. Alice Johnson",
-                lastname: "Dr. Alice Johnson",
-                specialty: "Cardiology",
-                profile: "https://randomuser.me/api/portraits/women/1.jpg",
-                role: "doctor",
-              },
-              {
-                id: "102",
-                firstname: "Dr. Mark Smith",
-                lastname: "Dr. Mark Smith",
-                specialty: "Neurology",
-                profile: "https://randomuser.me/api/portraits/men/2.jpg",
-                role: "doctor",
-              },
-              {
-                id: "103",
-                firstname: "Dr. Emily Davis",
-                lastname: "Dr. Emily Davis",
-                specialty: "Pediatrics",
-                profile: "https://randomuser.me/api/portraits/women/3.jpg",
-                role: "doctor",
-              },
-              {
-                id: "104",
-                firstname: "Dr. James Brown",
-                lastname: "Dr. James Brown",
-                specialty: "Orthopedics",
-                profile: "https://randomuser.me/api/portraits/men/4.jpg",
-                role: "doctor",
-              },
-              {
-                id: "105",
-                firstname: "Dr. Olivia Taylor",
-                lastname: "Dr. Olivia Taylor",
-                specialty: "Dermatology",
-                profile: "https://randomuser.me/api/portraits/women/5.jpg",
-                role: "doctor",
-              },
-            ]}
           />
         </Tab>
 
