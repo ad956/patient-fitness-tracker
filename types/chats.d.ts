560c45bfaf3a2633cc23235d0679cd7596c8e0a6
Anand Suthar <anandsuthar956@gmail.com>
2025-01-12 22:31:29 +0530

    feat: add chat-related interfaces for User, Message, Room, and components

--- types/chats.d.ts
+++ types/chats.d.ts
@@ -29,3 +29,58 @@ interface Room {
 interface ChatScreenProps {
   currentUser: User;
 }
+
+interface ChatModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  selectedRoom: Room;
+  currentUser: User;
+  messageList: Message[];
+  messagesLoading: boolean;
+  loadingMore: boolean;
+  onSendMessage: (message: string) => void;
+  onResend: (roomId: string, failedMessage: Message) => void;
+}
+
+interface ChatRoomListProps {
+  rooms: Room[];
+  currentUser: User;
+  onRoomSelect: (room: Room) => void;
+}
+
+interface EmptyStateProps {
+  roomsError: string;
+  fetchRoomsData: () => void;
+  setShowNewChatModal: () => void;
+}
+
+interface MessageComponentProps {
+  message: Message;
+  currentUserId: string;
+  onResend: () => void;
+}
+
+interface ChatListUser {
+  _id: string;
+  firstname: string;
+  lastname: string;
+  profile: string;
+}
+
+interface StartNewChatProps {
+  currentUserRole: string;
+  onChatRoomCreated: () => void;
+}
+
+export {
+  User,
+  Message,
+  Room,
+  ChatScreenProps,
+  ChatModalProps,
+  ChatRoomListProps,
+  EmptyStateProps,
+  MessageComponentProps,
+  ChatListUser,
+  StartNewChatProps,
+};
